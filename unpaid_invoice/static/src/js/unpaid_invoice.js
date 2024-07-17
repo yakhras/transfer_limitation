a87c4ae45947d6dd532718bca4b00b19d3cd9109
yakhras <58257996+yakhras@users.noreply.github.com>
2024-07-17 15:50:58 +0300

    edit js

--- unpaid_invoice/static/src/js/unpaid_invoice.js
+++ unpaid_invoice/static/src/js/unpaid_invoice.js
@@ -1,13 +1,19 @@
 odoo.define('export_pdf.print_pdf', function (require) {
   "use strict";
 
+  /* var ListController = require('web.ListController');
+   ListController.include({
+     renderButtons: function ($node) {
+       this._super.apply(this, arguments);
+       if (this.$buttons) {
+         this.$buttons.find('.o_list_export_pdf');
+       }
+     },
+   });
+ });*/
+
   var ListController = require('web.ListController');
   ListController.include({
-    renderButtons: function ($node) {
-      this._super.apply(this, arguments);
-      if (this.$buttons) {
-        this.$buttons.find('.o_list_export_pdf');
-      }
-    },
+    events: _.extend({}, ListController.prototype.events)
   });
 });
