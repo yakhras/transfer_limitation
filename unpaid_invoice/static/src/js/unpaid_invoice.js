0e9048d78b67aa996e77e41089a5551b2aa8e268
yakhras <58257996+yakhras@users.noreply.github.com>
2024-07-17 18:06:35 +0300

    edit

--- unpaid_invoice/static/src/js/unpaid_invoice.js
+++ unpaid_invoice/static/src/js/unpaid_invoice.js
@@ -1,19 +1,31 @@
-odoo.define('export_pdf.print_pdf', function (require) {
+odoo.define('button_near_create.tree_button', function (require) {
   "use strict";
-
-  /* var ListController = require('web.ListController');
-   ListController.include({
-     renderButtons: function ($node) {
-       this._super.apply(this, arguments);
-       if (this.$buttons) {
-         this.$buttons.find('.o_list_export_pdf');
-       }
-     },
-   });
- });*/
-
   var ListController = require('web.ListController');
-  ListController.include({
-    events: _.extend({}, ListController.prototype.events)
+  var ListView = require('web.ListView');
+  var viewRegistry = require('web.view_registry');
+  var TreeButton = ListController.extend({
+     buttons_template: 'button_near_create.buttons',
+     events: _.extend({}, ListController.prototype.events, {
+         'click .open_wizard_action': '_OpenWizard',
+     }),
+     _OpenWizard: function () {
+         var self = this;
+          this.do_action({
+             type: 'ir.actions.act_window',
+             res_model: 'test.wizard',
+             name :'Open Wizard',
+             view_mode: 'form',
+             view_type: 'form',
+             views: [[false, 'form']],
+             target: 'new',
+             res_id: false,
+         });
+     }
   });
-});
+  var SaleOrderListView = ListView.extend({
+     config: _.extend({}, ListView.prototype.config, {
+         Controller: TreeButton,
+     }),
+  });
+  viewRegistry.add('button_in_tree', SaleOrderListView);
+  });
