d8ffeda544011567889f75b576a77edc8120215f
yakhras <58257996+yakhras@users.noreply.github.com>
2024-07-18 18:49:30 +0300

    edit

--- unpaid_invoice/static/src/js/unpaid_invoice.js
+++ unpaid_invoice/static/src/js/unpaid_invoice.js
@@ -1,12 +1,11 @@
-odoo.define('button_near_create.tree_button', function (require) {
+odoo.define('owl_pos.call_button', function (require) {
     "use strict";
-    var rpc = require('web.rpc');
     var ListController = require('web.LoistController');
     ListController.include({
         events: _.extend({}, ListController.prototype.events, {
             "click .call_custom": "get_call_window",
         }),
-        get_call_window: function(e){
+        get_call_window: function(){
             console.log("Hello Yaser");
         },
     });
