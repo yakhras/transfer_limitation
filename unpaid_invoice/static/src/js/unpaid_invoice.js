1f87754f93d29ba21302f96be6a038ac61e460a3
yakhras <58257996+yakhras@users.noreply.github.com>
2024-07-19 10:07:25 +0300

    edi

--- unpaid_invoice/static/src/js/unpaid_invoice.js
+++ unpaid_invoice/static/src/js/unpaid_invoice.js
@@ -1,11 +1,13 @@
 odoo.define('owl_pos.call_button', function (require) {
     "use strict";
-    var ListController = require('web.LoistController');
+
+    console.log("Hi Yaser");
+    /*var ListController = require('web.LoistController');
     ListController.include({
         events: _.extend({}, ListController.prototype.events, {
             "click .call_custom": console.log("Hi Yaser"),
         }),
         
-    });
+    });*/
 });
     
