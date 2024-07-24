689932b75713251cd6b7a5ae099fd4b12c961a46
yakhras <58257996+yakhras@users.noreply.github.com>
2024-07-24 09:26:14 +0300

    edit

--- unpaid_invoice/__manifest__.py
+++ unpaid_invoice/__manifest__.py
@@ -21,7 +21,7 @@
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','account'],
 
     # always loaded
     'data': [
