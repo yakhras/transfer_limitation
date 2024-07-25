2226ecb33b05f572040a627905e5d3a9cf30fb48
yakhras <58257996+yakhras@users.noreply.github.com>
2024-07-25 11:08:58 +0300

    edit

--- unpaid_invoice/static/src/js/unpaid_invoice.js
+++ unpaid_invoice/static/src/js/unpaid_invoice.js
@@ -2,7 +2,7 @@ odoo.define('owl.call_button', function (require) {
     "use strict";
 
     const ListController = require('web.ListController');
-    const ListRenderer = require('web.ListRenderer')
+    //const ListRenderer = require('web.ListRenderer')
     const ListView = require('web.ListView');
     var viewRegistry = require('web.view_registry');
 
@@ -11,14 +11,19 @@ odoo.define('owl.call_button', function (require) {
     // Add click Event.
     const UnpaidListController = ListController.extend({
         buttons_template: "unpaid_button.buttons",
-        un_pai () {
+        events: _.extend({}, ListController.prototype.events, {
+            "click .call_unpaid": "get_call_unpaid",
+        }),
+        get_call_unpaid () {
+            console.log('Hi Yassor');
+            this.do_action('unpaid_invoice.unpaid_invoice_view_filter');
             console.log('Hi Yaser');
         },
 
     });
 
     // ListRenderer Inherits.
-    const UnpaidListRenderer = ListRenderer.extend({
+ /*   const UnpaidListRenderer = ListRenderer.extend({
         events: _.extend({}, ListRenderer.prototype.events, {
             "click .call_unpaid": "get_call_unpaid",
         }),
@@ -31,12 +36,11 @@ odoo.define('owl.call_button', function (require) {
             console.log('Hi Yassor');
         },
     });
-
+*/
     // ListView Inherits.
     const UnpaidListView = ListView.extend({
         config: _.extend({}, ListView.prototype.config, {
             Controller: UnpaidListController,
-            Renderer: UnpaidListRenderer,
         }),
     });
 
