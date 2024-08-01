ae53df3e8df6ae103f82fb889fff51d4f94d5c5e
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-01 15:31:45 +0300

    make comments

--- unpaid_invoice/__manifest__.py
+++ unpaid_invoice/__manifest__.py
@@ -21,23 +21,23 @@
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','sale'],
+    'depends': ['account'],
 
     # always loaded
     'data': [
         #"security/security.xml",
-        "reports.xml",
-        "menuitems.xml",
-        "report/layouts.xml",
-        "report/unpaid_invoice_report.xml",
+        "reports.xml", #action file.
+        "menuitems.xml", #menu items file.
+        "report/layouts.xml", #report page templates file. 
+        "report/unpaid_invoice_report.xml", #report data view file
     ],
 
     'assets': {
         'web.assets_qweb': [
-            'unpaid_invoice/static/src/xml/unpaid_invoice.xml',
+            'unpaid_invoice/static/src/xml/unpaid_invoice.xml', #buttons template file.
         ],
         'web.assets_backend': [
-            'unpaid_invoice/static/src/js/unpaid_invoice.js'
+            'unpaid_invoice/static/src/js/unpaid_invoice.js', #buttons event function file.
         ],
     },
     # only loaded in demonstration mode
