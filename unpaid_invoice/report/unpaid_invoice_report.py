0014c13ba085028f775a097f83f5ed527d1df066
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-02 12:41:34 +0300

    edt method params

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -17,7 +17,7 @@ class UnpaidInvoice(models.AbstractModel):
     #     # partners_data.update({"id":group_id, "name":group_name})
     #     return {'subjects':['Math', 'English', 'Programming']}
     
-    def _get_report_values(self, data):
+    def _get_report_values(self, docids, data):
         # partner_ids = data["partner_ids"]
         # partners_data = self.get_data(partner_ids)
         return {'subjects':['Math', 'English', 'Programming']}
