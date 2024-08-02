7d0022ca86772154846890656190156d952f1924
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-02 14:53:09 +0300

    edt method

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -21,9 +21,10 @@ class UnpaidInvoice(models.AbstractModel):
         # partner_ids = data["partner_ids"]
         # partners_data = self.get_data(partner_ids)
         partner = self.env['res.partner'].search([])
-        partner_id = str(partner.id)
-        partner_name = str(partner.name)
-        subjects = [partner_id, partner_name]
+        for part in partner:
+            partner_id = str(part.id)
+            partner_name = str(part.name)
+            subjects = [partner_id, partner_name]
         return {
             'subjects': subjects
         }
