5184b1606873de926578285b413c9ab73834788e
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-02 12:35:02 +0300

    edt method

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -9,20 +9,18 @@ class UnpaidInvoice(models.AbstractModel):
     _description = 'Unpaid Invoices Report'
 
     
-    def get_data(self, **kw):
-        # partners_data = {}
-        # partner = self.env["res.partner"]
-        # group_id = partner.id
-        # group_name = partner.name
-        # partners_data.update({"id":group_id, "name":group_name})
-        return {'subjects':['Math', 'English', 'Programming']}
+    # def get_data(self, **kw):
+    #     # partners_data = {}
+    #     # partner = self.env["res.partner"]
+    #     # group_id = partner.id
+    #     # group_name = partner.name
+    #     # partners_data.update({"id":group_id, "name":group_name})
+    #     return {'subjects':['Math', 'English', 'Programming']}
     
-    # def get_value(self, data):
-    #     partner_ids = data["partner_ids"]
-    #     partners_data = self.get_data(partner_ids)
-    #     return{
-    #         "partners_data": partners_data
-    #     }
+    def _get_report_values(self):
+        # partner_ids = data["partner_ids"]
+        # partners_data = self.get_data(partner_ids)
+        return {'subjects':['Math', 'English', 'Programming']}
         
     
 
