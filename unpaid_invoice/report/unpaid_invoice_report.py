2ff800e4cd6612ca47d2d492eb57cbb125b5ee80
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-02 11:39:11 +0300

    add another method

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -9,13 +9,20 @@ class UnpaidInvoice(models.AbstractModel):
     _description = 'Unpaid Invoices Report'
 
     
-    def yaser(self):
+    def get_data(self, **kw):
         partners_data = {}
         partner = self.env["res.partner"]
         group_id = partner.id
         group_name = partner.name
         partners_data.update({group_id: {"id":group_id, "name":group_name}})
         return partners_data
+    
+    def get_value(self, data):
+        partner_ids = data["partner_ids"]
+        partners_data = self.get_data(partner_ids)
+        return{
+            "partners_data": partners_data
+        }
         
     
 
