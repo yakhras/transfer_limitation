8fb0241d0cda67735b9e29dcdfd41ebc19e6da12
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-06 12:14:06 +0300

    add id

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -28,7 +28,7 @@ class UnpaidInvoice(models.AbstractModel):
                 ('partner_id.property_account_receivable_id.code', '=', '120001')
         ]
         records =[]
-        idd = self.env['res.partner'].search([]).id
+        idd = self.env['res.partner'].search([]).id.id
         for i in idd:
         # table = self.env['account.move'].search(domain).filtered(lambda x: x.partner_id.id == i)
             records.append(i)
