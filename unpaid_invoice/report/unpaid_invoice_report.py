87e78e8706e6a3f8dee76da6c124105c6eb36187
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-07 13:02:55 +0300

    edt match

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -38,7 +38,7 @@ class UnpaidInvoice(models.AbstractModel):
 
         match = {}
         for r in records.keys():
-            match.update(records[r]['id'])
+            match.update({"id":records[r]['id']})
         
         return {
             'invoices': invoices,
