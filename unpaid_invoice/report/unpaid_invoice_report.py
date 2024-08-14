80d5275f7de84e6ab064a886ec486b84283a4f98
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-14 16:30:29 +0300

    edt code

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -55,7 +55,7 @@ class UnpaidInvoice(models.AbstractModel):
         #     'match': match,
         # }
         return {
-            'today': model,
+            # 'today': model,
             'domain': docids,
         }
         
