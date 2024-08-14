c3056b4c836c5b138b132edb65074be82e9a96bc
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-14 16:42:24 +0300

    edt code

--- unpaid_invoice/report/unpaid_invoice_report.py
+++ unpaid_invoice/report/unpaid_invoice_report.py
@@ -16,7 +16,6 @@ class UnpaidInvoice(models.AbstractModel):
         if (docids[0] == 1):
             code = 120001
         # Define domain for search
-        
         domain = [
             ('move_type', '=', 'out_invoice'),
                 ('state', '=', 'posted'),
@@ -24,40 +23,40 @@ class UnpaidInvoice(models.AbstractModel):
                 ('invoice_date_due', '<', today.strftime('%Y-%m-%d')),
                 ('partner_id.property_account_receivable_id.code', '=', code)
         ]
-        # # Define dictionary for partners
-        # partners = {}
-        # # Define dictionary for invoices
-        # invoices = {}
-        # # Define table will working with
-        # table = self.env['account.move'].search(domain)
-        # # Extract data from table to update the dictionaries
-        # for raw in table:
-        #     partner_id = raw.partner_id #res.partner object
-        #     inv_id = raw.id #account.move id
-        #     inv_pay_ref = raw.payment_reference #payment reference / invoice number
-        #     inv_due_date = raw.invoice_date_due #.strftime('%Y-%m-%d') #invoice due date
-        #     delay = (today - inv_due_date).days #delay duration from due date to now
-        #     part_name = partner_id.name #partner name
-        #     part_team = partner_id.team_id.name #partner team
-        #     invoices.update({inv_id: {"id":partner_id, "pr":inv_pay_ref, "pn":part_name, "dt":inv_due_date, "dd":delay, "pt":part_team}}) #update dict with value
-        #     partners.update({partner_id:{"id":partner_id, "name":part_name}}) #update dict with value
-        #     # Define a dictionary contains matched values, will return nested dict
-        # match = {j['id']:
-        #         {
-        #             z:{"ref":d['pr'], "partner":d['pn'], "date":d['dt'], "delay":d['dd'], "team":d['pt']}
-        #             for (z,d) in invoices.items()
-        #             if j['id']==d['id']
-        #         }
-        #         for (i,j) in partners.items()
-        #         }
+        # Define dictionary for partners
+        partners = {}
+        # Define dictionary for invoices
+        invoices = {}
+        # Define table will working with
+        table = self.env['account.move'].search(domain)
+        # Extract data from table to update the dictionaries
+        for raw in table:
+            partner_id = raw.partner_id #res.partner object
+            inv_id = raw.id #account.move id
+            inv_pay_ref = raw.payment_reference #payment reference / invoice number
+            inv_due_date = raw.invoice_date_due #.strftime('%Y-%m-%d') #invoice due date
+            delay = (today - inv_due_date).days #delay duration from due date to now
+            part_name = partner_id.name #partner name
+            part_team = partner_id.team_id.name #partner team
+            invoices.update({inv_id: {"id":partner_id, "pr":inv_pay_ref, "pn":part_name, "dt":inv_due_date, "dd":delay, "pt":part_team}}) #update dict with value
+            partners.update({partner_id:{"id":partner_id, "name":part_name}}) #update dict with value
+            # Define a dictionary contains matched values, will return nested dict
+        match = {j['id']:
+                {
+                    z:{"ref":d['pr'], "partner":d['pn'], "date":d['dt'], "delay":d['dd'], "team":d['pt']}
+                    for (z,d) in invoices.items()
+                    if j['id']==d['id']
+                }
+                for (i,j) in partners.items()
+                }
     
-        # return {
-        #     'invoices': invoices,
-        #     'partners': partners,
-        #     'match': match,
-        # }
         return {
-            # 'today': model,
-            'domain': domain,
+            'invoices': invoices,
+            'partners': partners,
+            'match': match,
         }
+        # return {
+        #     # 'today': model,
+        #     'domain': domain,
+        # }
         
