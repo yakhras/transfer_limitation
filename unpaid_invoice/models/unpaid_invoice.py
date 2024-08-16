de9f0ea92bb3a93fc8eb2ab9d043d4c7ae24c53e
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-16 15:15:40 +0300

    add field

--- unpaid_invoice/models/unpaid_invoice.py
+++ unpaid_invoice/models/unpaid_invoice.py
@@ -3,6 +3,11 @@ from odoo import models, fields
 class CrmTeam(models.Model):
     _inherit = 'crm.team'
 
+    label_date = fields.Char(
+        default=lambda s: _("Tickets"),
+        translate=True,
+        )
+
     def action_unpaid_invoice(self):
          action = self.env["ir.actions.actions"]._for_xml_id(
               "unpaid_invoice.action_report_unpaid_invoice_html")
