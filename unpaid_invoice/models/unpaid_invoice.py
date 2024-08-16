f3adc54a53a500ebd67b8c0cd53b192e92df26e6
yakhras <58257996+yakhras@users.noreply.github.com>
2024-08-16 16:29:49 +0300

    add counter

--- unpaid_invoice/models/unpaid_invoice.py
+++ unpaid_invoice/models/unpaid_invoice.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import date
 
 class CrmTeam(models.Model):
     _inherit = 'crm.team'
@@ -7,7 +8,19 @@ class CrmTeam(models.Model):
         default=lambda s: "Today",
         translate=True,
         )
-
+    
+    def _count_records(self):
+          today = date.today()
+          domain = [
+            ('move_type', '=', 'out_invoice'),
+                ('state', '=', 'posted'),
+                ('payment_state', 'in', ('not_paid', 'partial')),
+                ('invoice_date_due', '=', today.strftime('%Y-%m-%d')),
+                ('partner_id.property_account_receivable_id.code', '=', 120001)
+        ]
+          total_count = self.env['account.move'].search_count(domain)
+          return total_count
+                
     def action_unpaid_invoice(self):
          action = self.env["ir.actions.actions"]._for_xml_id(
               "unpaid_invoice.action_report_unpaid_invoice_html")
