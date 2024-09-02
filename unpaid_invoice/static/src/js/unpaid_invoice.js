cea7f9374c462d84feb02c83cde15e04a182e9a0
yakhras <it@menagate.com.tr>
2024-09-02 18:32:52 +0300

    edt action

--- unpaid_invoice/static/src/js/unpaid_invoice.js
+++ unpaid_invoice/static/src/js/unpaid_invoice.js
@@ -1,7 +1,8 @@
 odoo.define('owl.call_button', function (require) {
     "use strict";
 
-    const ReportAction = require('report.client_action');
+    var AbstractAction = require('web.AbstractAction');
+    // const ReportAction = require('report.client_action');
     var core = require('web.core');
     // const ListView = require('web.ListView');
     // var viewRegistry = require('web.view_registry');
@@ -9,44 +10,57 @@ odoo.define('owl.call_button', function (require) {
     // ListController Inherites,
     // Add Button Template,
     // Add click Event.
-    const UnpaidReportAction = ReportAction.extend({
+    var ksDynamicReportsWidget = AbstractAction.extend({
         hasControlPanel: true,
-        contentTemplate: 'report.unpaid_invoice',
-
-        init: function (parent, action, options) {
-            this._super.apply(this, arguments);
-        },
-
-        start: function () {
-            var self = this;
-            this.iframe = this.$('iframe')[0];
-            this.$buttons = $(QWeb.render('unpaid_button.buttons', {}));
-            this.$buttons.on('click', '.call_unpaid', this.call_unpaid);
-            this.controlPanelProps.cp_content = {
-                $buttons: this.$buttons,
-            };
-        },
-        // buttons_template: "unpaid_button.buttons123",
-        // events: _.extend({}, ListController.prototype.events, {
-        //     "click .call_unpaid": "get_call_unpaid",
-        // }),
-        // get_call_unpaid () {
-        //     console.log('Hi Yassor');
-        //     this.do_action('unpaid_invoice.unpaid_invoices_action');
-        //     console.log('Hi Yaser');
-        // },
+        events: {
+            'click .ks_report_pdf': 'ksReportPrintPdf',
+            'click .ks_report_xlsx': 'ksPrintReportXlsx',
+            'click .ks_send_email': "ksReportSendEmail",
+        }
     })
 
-    // ListView Inherits.
-    // const UnpaidListView = ListView.extend({
-    //     config: _.extend({}, ListView.prototype.config, {
-    //         Controller: UnpaidReportAction,
-    //     }),
+
+    core.action_registry.add('ks_dynamic_report', ksDynamicReportsWidget);
+    return ksDynamicReportsWidget;
+
+});
+    // const UnpaidReportAction = ReportAction.extend({
+    //     hasControlPanel: true,
+    //     contentTemplate: 'report.unpaid_invoice',
+
+    //     init: function (parent, action, options) {
+    //         this._super.apply(this, arguments);
+    //     },
+
+    //     start: function () {
+    //         var self = this;
+    //         this.iframe = this.$('iframe')[0];
+    //         this.$buttons = $(QWeb.render('unpaid_button.buttons', {}));
+    //         this.$buttons.on('click', '.call_unpaid', this.call_unpaid);
+    //         this.controlPanelProps.cp_content = {
+    //             $buttons: this.$buttons,
+    //         };
+    //     },
+    //     // buttons_template: "unpaid_button.buttons123",
+    //     // events: _.extend({}, ListController.prototype.events, {
+    //     //     "click .call_unpaid": "get_call_unpaid",
+    //     // }),
+    //     // get_call_unpaid () {
+    //     //     console.log('Hi Yassor');
+    //     //     this.do_action('unpaid_invoice.unpaid_invoices_action');
+    //     //     console.log('Hi Yaser');
+    //     // },
     // })
 
-    // Register ListView with js Class
-    // viewRegistry.add('unpaid_list', UnpaidListView);
+    // // ListView Inherits.
+    // // const UnpaidListView = ListView.extend({
+    // //     config: _.extend({}, ListView.prototype.config, {
+    // //         Controller: UnpaidReportAction,
+    // //     }),
+    // // })
 
-    core.action_registry.add('report.unpaid_invoice', UnpaidReportAction);
-    return UnpaidReportAction;
-});
+    // // Register ListView with js Class
+    // // viewRegistry.add('unpaid_list', UnpaidListView);
+
+    // core.action_registry.add('report.unpaid_invoice', UnpaidReportAction);
+    // return UnpaidReportAction;
