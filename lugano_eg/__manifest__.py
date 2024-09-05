4807342489cb8bd8714025068cba99d4f157fa84
yakhras <it@menagate.com.tr>
2024-09-05 12:59:09 +0300

    add category

--- lugano_eg/__manifest__.py
+++ lugano_eg/__manifest__.py
@@ -15,6 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    'category': 'Lugano/Survey',
     
     'version': '15.0.1.0',
     'application': True,
