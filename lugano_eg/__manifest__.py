7b3ea035c86d45a20b6c58acbc39d45620a7c468
yakhras <it@menagate.com.tr>
2024-09-05 12:08:39 +0300

    add new app

---
+++ lugano_eg/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "lugano_eg",
+
+    'summary': """
+        Lugano Coffee Egypt.""",
+
+    'description': """
+        Lugano Coffee Egypt.. 
+    """,
+
+    'author': "Yaser Akhras",
+    #'website': "https://www.yaserakhras.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    
+    'version': '15.0.1.0',
+    'application': True,
+    'license': 'AGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        #"security/security.xml",
+        # "security/ir.model.access.csv",
+        # "wizard/transfer_limi_wizard_view.xml",
+        # "views/res_partner_view.xml",
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ lugano_eg/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import transfer_limit_wizard
