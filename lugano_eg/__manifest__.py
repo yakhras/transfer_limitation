e2b792ad711b9bfde678cc971cf6559d2e2b3042
yakhras <it@menagate.com.tr>
2024-09-05 12:55:36 +0300

    edt security

--- lugano_eg/__manifest__.py
+++ lugano_eg/__manifest__.py
@@ -25,9 +25,9 @@
 
     # always loaded
     'data': [
-        #"security/security.xml",
+        # "security/security.xml",
         "security/logistics_security.xml",
-        # "security/ir.model.access.csv",
+        "security/ir.model.access.csv",
         # "wizard/transfer_limi_wizard_view.xml",
         "views/lugano_eg_menu_view.xml",
     ],
