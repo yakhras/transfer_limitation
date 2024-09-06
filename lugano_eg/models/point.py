c186966bc8ff9c965c526746fc5a4a1c2788f0a9
yakhras <it@menagate.com.tr>
2024-09-06 10:25:43 +0300

    add field

--- lugano_eg/models/point.py
+++ lugano_eg/models/point.py
@@ -26,6 +26,7 @@ class LuganoPoint(models.Model):
     pod_consume = fields.Char()
     capsules = fields.Boolean(string='Capsules')
     capsules_consume = fields.Char()
+    used_price = fields.Char()
     machine_station = fields.Selection(
         [("no_need", "No Need"),
          ("repair", "Need Repair"),
