c485ddc56487c76f3186489b9804522d5f99fcf6
yakhras <it@menagate.com.tr>
2024-09-06 12:22:32 +0300

    edt attr

--- lugano_eg/models/point.py
+++ lugano_eg/models/point.py
@@ -13,7 +13,7 @@ class LuganoPoint(models.Model):
 
     READONLYSTATES = {'done': [('readonly', True)], 'cancel': [('readonly', True)]}
 
-    name = fields.Char(string='Point Title')
+    name = fields.Char(string='Point Title' ,states=READONLYSTATES)
     number = fields.Char(string='Number', required=True, readonly=True, default="/")
     state = fields.Selection([
         ('draft', 'Draft'),
