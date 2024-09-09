0e6b17672c5d492be348aeff5903361708927fc9
yakhras <it@menagate.com.tr>
2024-09-09 18:16:56 +0300

    del images

--- lugano_eg/__manifest__.py
+++ lugano_eg/__manifest__.py
@@ -1,20 +1,5 @@
 # -*- coding: utf-8 -*-
-#╔══════════════════════════════════════════════════════════════════════╗
-#║                                                                      ║
-#║                  ╔═══╦╗       ╔╗  ╔╗     ╔═══╦═══╗                   ║
-#║                  ║╔═╗║║       ║║ ╔╝╚╗    ║╔═╗║╔═╗║                   ║
-#║                  ║║ ║║║╔╗╔╦╦══╣╚═╬╗╔╬╗ ╔╗║║ ╚╣╚══╗                   ║
-#║                  ║╚═╝║║║╚╝╠╣╔╗║╔╗║║║║║ ║║║║ ╔╬══╗║                   ║
-#║                  ║╔═╗║╚╣║║║║╚╝║║║║║╚╣╚═╝║║╚═╝║╚═╝║                   ║
-#║                  ╚╝ ╚╩═╩╩╩╩╩═╗╠╝╚╝╚═╩═╗╔╝╚═══╩═══╝                   ║
-#║                            ╔═╝║     ╔═╝║                             ║
-#║                            ╚══╝     ╚══╝                             ║
-#║                  SOFTWARE DEVELOPED AND SUPPORTED BY                 ║
-#║                ALMIGHTY CONSULTING SOLUTIONS PVT. LTD.               ║
-#║                      COPYRIGHT (C) 2016 - TODAY                      ║
-#║                      https://www.almightycs.com                      ║
-#║                                                                      ║
-#╚══════════════════════════════════════════════════════════════════════╝
+
 {
     'name' : 'Lugano Egypt',
     'category' : 'Project',
@@ -29,15 +14,8 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'data/visit_data.xml',
-        # 'views/project_views.xml',
         'views/visit_view.xml',
-        # 'reports/visit_report.xml',
-    ],
-    'images': [
-        'static/description/odoo_visits_almightycs_cover.jpg',
     ],
     'application': True,
     'sequence': 1,
-    'price': 36,
-    'currency': 'USD',
 }
