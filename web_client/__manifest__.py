2ab444c02064ea5ed7b3029bd4db4a188ae243f1
yakhras <it@menagate.com.tr>
2024-09-16 16:05:41 +0300

    edt version

--- web_client/__manifest__.py
+++ web_client/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     
-    'version': '15.0.1.0',
+    'version': '17.0.1.0',
     'application': True,
     'license': 'AGPL-3',
 
