c11164840b1e0369f15938fdfc8bec5504a4f58f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-01 12:59:06 +0300

    add a new addon

---
+++ email/models/email_market.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+from datetime import date, timedelta
+from odoo.exceptions import ValidationError
+
+
+
+class MailingMailing(models.Model):
+    _inherit = 'mailing.mailing'   # Inherit the model
+
+    email = fields.Boolean(string='email reciept')
+
+    @api.onchange('email')
+    def _onchange_email(self):
+        if (self.email):
+            self.copyvalue = self.contact_list_ids
+        else:
+            self.copyvalue = 0.0
