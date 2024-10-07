bc78dd08153af2c5be2375f048fb6154087d3bc4
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-07 11:33:05 +0000

    test cancel

--- sale_draft/models/sale_draft.py
+++ sale_draft/models/sale_draft.py
@@ -11,10 +11,12 @@ class SaleOrder(models.Model):
 
     @api.onchange('purchase')
     def _onchange_purchase(self):
-        order = self._get_purchase_orders()
+        #order = self._get_purchase_orders()
         if (self.purchase):
-            self.client_order_ref = order.name
+            self.action_cancel()
+            #self.client_order_ref = order.name
         else:
-            order.button_cancel()
-            order.unlink()
-            self.client_order_ref = self.qty_delivered
+            # order.button_cancel()
+            # order.unlink()
+            # self.client_order_ref = self.qty_delivered
+            self.client_order_ref = 'Yes'
