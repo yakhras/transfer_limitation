7e5cdbd1c9e17daed0297accf69c235e84ed6196
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-10 14:23:45 +0000

    add self.id

--- partner_balance/models/partner_balance.py
+++ partner_balance/models/partner_balance.py
@@ -16,6 +16,6 @@ class ResPartner(models.Model):
 
     def get_balance(self):
         acmvln = self.env['account.move.line']
-        filter = acmvln.filtered(lambda x: x.partner_id)
+        filter = acmvln.filtered(lambda x: x.partner_id == self.id)
         return filter
        
