697e090777376408f01daa1efd3b2e65d2383599
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-11 09:30:07 +0000

    edt

--- partner_balance/models/partner_balance.py
+++ partner_balance/models/partner_balance.py
@@ -12,16 +12,21 @@ class ResPartner(models.Model):
 
     @api.onchange('balance_id')
     def on_change_balance_id(self):
-        self.balance = self.get_balance()
+        list = sef.get_balance()
+        self.balance = self.total_debit(list)
 
     def get_balance(self):
         # acmvln = self.env['account.move.line'].search([])
         # filter = acmvln.filtered(lambda x: x.partner_id == self.id and reconciled == False)
         ids = []
-        total_debit = 0
         for one in self.move_line_ids:
             ids.append(one.debit)
-            for id in ids:
-                total_debit += id
+        return ids
+
+    def total_debit(self, list):
+        total_debit = 0
+        for number in list:
+            total_debit += number
         return total_debit
+
        
