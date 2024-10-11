ce8f2dd7a12b3c9ad3e43ed69a90dee8dc99baf2
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-11 08:41:30 +0000

    id

--- partner_balance/models/partner_balance.py
+++ partner_balance/models/partner_balance.py
@@ -18,8 +18,6 @@ class ResPartner(models.Model):
         # acmvln = self.env['account.move.line'].search([])
         # filter = acmvln.filtered(lambda x: x.partner_id == self.id and reconciled == False)
         movlin = self.move_line_ids.filtered(lambda x: x.reconciled)
-        debit = 0.0
         for one in movlin:
-            debit = debit + one.debit
-        return debit
+            return one.id
        
