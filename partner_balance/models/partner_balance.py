31170ffd26c7892b391fbde7d418d94cebd9b414
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-11 08:48:26 +0000

    edt

--- partner_balance/models/partner_balance.py
+++ partner_balance/models/partner_balance.py
@@ -17,7 +17,5 @@ class ResPartner(models.Model):
     def get_balance(self):
         # acmvln = self.env['account.move.line'].search([])
         # filter = acmvln.filtered(lambda x: x.partner_id == self.id and reconciled == False)
-        movlin = self.move_line_ids.filtered(lambda x: x.reconciled == True)
-        for one in movlin:
-            return one.id
+        return self.move_line_ids
        
