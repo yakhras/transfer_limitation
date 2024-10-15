67609e73bbff849a69b130595662c755f2bf971a
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-15 14:01:57 +0000

    edt

--- pos_invoice/__manifest__.py
+++ pos_invoice/__manifest__.py
@@ -14,11 +14,7 @@
     ],
     'assets': {
         'point_of_sale.assets': [
-            #'wt_create_so_from_pos/static/src/js/Screens/ProductScreen/ControlButtons/SaleOrderButton.js',
-            #'wt_create_so_from_pos/static/src/js/Screens/ProductScreen/ControlButtons/ViewSalesOrderButton.js',
             'pos_invoice/static/src/js/Screens/SaleOrderScreen/SaleOrderScreen.js',
-            #'wt_create_so_from_pos/static/src/js/Screens/SaleOrderScreen/ViewSaleOrderList.js',
-            #'wt_create_so_from_pos/static/src/js/Screens/SaleOrderScreen/ViewSaleOrderRow.js',
             'pos_invoice/static/src/js/Popups/SalesOrderPopup.js',
         ],
         'web.assets_qweb': [
