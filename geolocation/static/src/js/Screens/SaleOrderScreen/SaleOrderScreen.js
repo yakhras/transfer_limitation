e35b3385c6d79b481fca5669027e1817b2535c2f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-21 14:16:28 +0000

    edt

--- geolocation/static/src/js/Screens/SaleOrderScreen/SaleOrderScreen.js
+++ geolocation/static/src/js/Screens/SaleOrderScreen/SaleOrderScreen.js
@@ -35,7 +35,7 @@ odoo.define('geolocation.getLocation', function (require) {
                 if (confirmed) {
                     if (selectedOption) {
                         console.log('True');
-                        navigator.geolocation.getCurrentPosition(function(position) {
+                        navigator.geolocation.getCurrentPosition(function(position){
                             // const ctx = Object.assign(session.user_context, {
                             //     latitude: position.coords.latitude,
                             //     longitude: position.coords.longitude,
