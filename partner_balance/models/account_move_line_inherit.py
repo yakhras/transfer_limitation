b403a5b0f27b4b64deb4b4e6bed32ed057a850ad
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-23 16:28:37 +0300

    edt

--- partner_balance/models/account_move_line_inherit.py
+++ partner_balance/models/account_move_line_inherit.py
@@ -28,3 +28,23 @@ class AccountMoveLine(models.Model):
                 partner_balance._compute_balance()  # Call the method to recalculate balance
 
         return res
+    
+    @api.model
+    def create(self, vals):
+        # Create the account.move.line record
+        move_line = super(AccountMoveLine, self).create(vals)
+
+        # Check if the relevant fields are in the created values
+        if any(field in vals for field in ['debit', 'credit', 'full_reconcile_id']):
+            partner_id = move_line.partner_id.id
+            
+            # Log the partner being updated
+            _logger.info("Creating account.move.line and updating balance for partner_id: %s", partner_id)
+            
+            # Recalculate balances for the affected partner balance
+            partner_balance = self.env['partner.balance'].search([('partner_id', '=', partner_id)], limit=1)
+            if partner_balance:
+                _logger.info("Recalculating balance for partner: %s", partner_balance.partner_id.name)
+                partner_balance.get_balance_value()  # Call the method to recalculate balance
+
+        return move_line
