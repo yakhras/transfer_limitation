5425cb1fd43c4f535c0f430c8d990e4e4a58d5bc
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-23 16:01:25 +0300

    edt

--- partner_balance/models/account_move_line_inherit.py
+++ partner_balance/models/account_move_line_inherit.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, api
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
@@ -14,12 +17,14 @@ class AccountMoveLine(models.Model):
         if any(field in vals for field in ['debit', 'credit', 'full_reconcile_id']):
             # Get the partner_id from the move lines being updated
             partner_ids = self.mapped('partner_id').ids
+            _logger.info("Updating partner balances for partner_ids: %s", partner_ids)
 
             # Recalculate balances for affected partner balances
             partner_balances = self.env['partner.balance'].search([
                 ('partner_id', 'in', partner_ids)
             ])
             for partner_balance in partner_balances:
+                _logger.info("Recalculating balance for partner: %s", partner_balance.partner_id.name)
                 partner_balance._compute_balance()  # Call the method to recalculate balance
 
         return res
