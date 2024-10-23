a3fb5ef3ad04a8423d186cbb036072ae40103179
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-23 15:34:09 +0300

    edt

--- partner_balance/models/account_move_line_inherit.py
+++ partner_balance/models/account_move_line_inherit.py
@@ -5,14 +5,13 @@ from odoo import models, api
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    
     @api.model
     def write(self, vals):
         # Call the original write method
         res = super(AccountMoveLine, self).write(vals)
 
         # Check if the relevant fields are in the updated values
-        if any(field in vals for field in ['debit', 'credit', 'full_reconcile_id']):
+        if 'debit' in vals or 'credit' in vals :
             # Get the partner_id from the move lines being updated
             partner_ids = self.mapped('partner_id').ids
 
@@ -21,6 +20,6 @@ class AccountMoveLine(models.Model):
                 ('partner_id', 'in', partner_ids)
             ])
             for partner_balance in partner_balances:
-                partner_balance.get_balance_value()  # Call the method to recalculate balance
+                partner_balance._compute_balance()  # Call the method to recalculate balance
 
         return res
