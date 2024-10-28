f718495770e0070e1a1dc04bffda1d62b7c3484e
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-28 14:41:27 +0300

    edt

--- export_view_pdf/controllers/main.py
+++ export_view_pdf/controllers/main.py
@@ -25,45 +25,45 @@ from odoo.http import request
 from odoo.exceptions import UserError
 
 
-class ExportData(http.Controller):
-    @http.route('/get_data', auth="user", type='json')
-    def get_export_data(self, **kw):
-        """
-        controller to fetch required retails
-        """
-        field = kw['field']
-        model = kw['model']
-        Model = request.env[model]
-        res_ids = kw['res_ids']
-        field_label = kw['exported_fields']
-        columns_headers = [val['label'].strip() for val in field_label]
-        records = request.env[model].browse(res_ids)
+# class ExportData(http.Controller):
+#     @http.route('/get_data', auth="user", type='json')
+#     def get_export_data(self, **kw):
+#         """
+#         controller to fetch required retails
+#         """
+#         field = kw['field']
+#         model = kw['model']
+#         Model = request.env[model]
+#         res_ids = kw['res_ids']
+#         field_label = kw['exported_fields']
+#         columns_headers = [val['label'].strip() for val in field_label]
+#         records = request.env[model].browse(res_ids)
 
-        if kw['grouped_by']:
-            field_names = [f for f in kw['field']]
-            groupby_type = [Model._fields[x.split(':')[0]].type for x in
-                            kw['grouped_by']]
-            domain = kw['domain']
+#         if kw['grouped_by']:
+#             field_names = [f for f in kw['field']]
+#             groupby_type = [Model._fields[x.split(':')[0]].type for x in
+#                             kw['grouped_by']]
+#             domain = kw['domain']
 
-            groups_data = Model.sudo().read_group(domain,
-                                                  field_names, kw['grouped_by'],
-                                                  lazy=False)
-            group_by = []
-            for rec in groups_data:
-                ids = Model.search(rec['__domain'])
-                list_key = [x for x in rec.keys() if
-                            x in kw['field'] and x not in kw['grouped_by']]
-                new_export_data = [ids.export_data(field).get('datas', [])]
-                group_tuple = (
-                    {'count': rec['__count']}, rec.get(kw['grouped_by'][0]),
-                    new_export_data,
-                    [(rec[x], field.index(x)) for x in list_key])
-                group_by.append(group_tuple)
-            return {'header': columns_headers, 'data': new_export_data,
-                    'type': groupby_type, 'other': group_by}
-        else:
-            new_export_data = records.export_data(field).get('datas', [])
-            return {'data': new_export_data, 'header': columns_headers}
+#             groups_data = Model.sudo().read_group(domain,
+#                                                   field_names, kw['grouped_by'],
+#                                                   lazy=False)
+#             group_by = []
+#             for rec in groups_data:
+#                 ids = Model.search(rec['__domain'])
+#                 list_key = [x for x in rec.keys() if
+#                             x in kw['field'] and x not in kw['grouped_by']]
+#                 new_export_data = [ids.export_data(field).get('datas', [])]
+#                 group_tuple = (
+#                     {'count': rec['__count']}, rec.get(kw['grouped_by'][0]),
+#                     new_export_data,
+#                     [(rec[x], field.index(x)) for x in list_key])
+#                 group_by.append(group_tuple)
+#             return {'header': columns_headers, 'data': new_export_data,
+#                     'type': groupby_type, 'other': group_by}
+#         else:
+#             new_export_data = records.export_data(field).get('datas', [])
+#             return {'data': new_export_data, 'header': columns_headers}
 
 
 
@@ -141,4 +141,60 @@ class ExportData(http.Controller):
 #             'type': [model._fields[f.split(':')[0]].type for f in grouped_by_fields],
 #             'other': grouped_export_data
 #         }
+
+
+
+
+class ExportData(http.Controller):
+
+    @http.route('/get_data', auth="user", type='json')
+    def fetch_exported_data(self, **kwargs):
+        """
+        Controller to fetch the required details.
+        """
+        model = kwargs['model']
+        fields = kwargs['field']
+        res_ids = kwargs['res_ids']
+        field_labels = kwargs['exported_fields']
+        columns_headers = [val['label'].strip() for val in field_labels]
+        
+        Model = request.env[model]
+        records = Model.browse(res_ids)
+
+        if kwargs['grouped_by']:
+            return self._fetch_grouped_data(Model, fields, kwargs, columns_headers)
+        else:
+            export_data = records.export_data(fields).get('datas', [])
+            return {'data': export_data, 'header': columns_headers}
+
+    def _fetch_grouped_data(self, Model, fields, kwargs, columns_headers):
+        """
+        Fetch and group the data based on the grouped_by fields.
+        """
+        field_names = [f for f in fields]
+        groupby_fields = kwargs['grouped_by']
+        domain = kwargs['domain']
+        
+        groupby_type = [Model._fields[x.split(':')[0]].type for x in groupby_fields]
+        groups_data = Model.sudo().read_group(domain, field_names, groupby_fields, lazy=False)
+        
+        grouped_result = []
+        for group in groups_data:
+            record_ids = Model.search(group['__domain'])
+            field_keys = [k for k in group.keys() if k in fields and k not in groupby_fields]
+            export_data = [record_ids.export_data(fields).get('datas', [])]
+            group_info = (
+                {'count': group['__count']},
+                group.get(groupby_fields[0]),
+                export_data,
+                [(group[k], fields.index(k)) for k in field_keys]
+            )
+            grouped_result.append(group_info)
+        
+        return {
+            'header': columns_headers,
+            'data': export_data,
+            'type': groupby_type,
+            'other': grouped_result
+        }
 
