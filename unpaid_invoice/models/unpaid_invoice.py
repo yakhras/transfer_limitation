8610cc8b336f79f50ac69459205ca39c2af41947
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-30 09:32:33 +0300

    edt

--- unpaid_invoice/models/unpaid_invoice.py
+++ unpaid_invoice/models/unpaid_invoice.py
@@ -4,27 +4,12 @@
 # class CrmTeam(models.Model):
 #     _inherit = 'crm.team'
 
-#     label_date = fields.Char(
-#         default=lambda s: "Today",
-#         translate=True,
-#         ) 
 #     total_count = fields.Integer(compute="_count_records", store=True)
     
 #     def _count_records(self):
-#           today = date.today()
-#           domain = [
-#             ('move_type', '=', 'out_invoice'),
-#                 ('state', '=', 'posted'),
-#                 ('payment_state', 'in', ('not_paid', 'partial')),
-#                 ('invoice_date_due', '=', today.strftime('%Y-%m-%d')),
-#                 ('partner_id.property_account_receivable_id.code', '=', 120001)
-#         ]
 #           self.total_count = self.env['account.move'].search_count(domain)
                 
-#     def action_unpaid_invoice(self):
-#          action = self.env["ir.actions.actions"]._for_xml_id(
-#               "unpaid_invoice.action_report_unpaid_invoice_html")
-#          return action
+
     
 from odoo import models, fields, api
 
@@ -33,6 +18,7 @@ class UnpaidInvoice(models.Model):
     _description = 'Unpaid Invoices for Sales Teams'
 
     invoice_id = fields.Many2one('account.move', string='Invoice', required=True)
+    document_id = fields.Char(related='invoice_id.document_number', string='Invoice')
     partner_id = fields.Many2one(related='invoice_id.partner_id', string="Customer")
     amount_due = fields.Monetary(related='invoice_id.amount_residual', string="Amount Due")
     due_date = fields.Date(related='invoice_id.invoice_date_due', string="Due Date")
