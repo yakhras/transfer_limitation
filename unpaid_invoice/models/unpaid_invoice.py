2987028ca4be88f7d40214582b571a0937ecff76
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-30 11:37:09 +0300

    edt

--- unpaid_invoice/models/unpaid_invoice.py
+++ unpaid_invoice/models/unpaid_invoice.py
@@ -65,4 +65,4 @@ class UnpaidInvoice(models.Model):
     def send_email_unpaid_invoices(self):
         template = self.env.ref('unpaid_invoice.unpaid_invoice')
         for rec in self:
-            template.send_mail(rec.id)
+            template.send_mail(rec.id, force_send=True)
