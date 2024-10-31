9d0ab21b4dc80983bc0e1d4e8cb304abd644f151
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-10-31 16:51:09 +0300

    edt

--- unpaid_invoice/static/src/js/pdf_export.js
+++ unpaid_invoice/static/src/js/pdf_export.js
@@ -11,48 +11,103 @@ ListController.include({
          this.$buttons.find('.o_list_export_pdf').click(this.proxy('action_def')) ;
        }
    },
+    // action_def: function () {
+    // var self=this;
+    // var fields=this._getExportDialogWidget().defaultExportFields;
+    // let exported_fields = this._getExportDialogWidget().defaultExportFields.map(field => ({
+    //         name: field,
+    //         label: this._getExportDialogWidget().record.fields[field].string,
+    //     }));
+    // let groupedBy = this.renderer.state.groupedBy;
+    // var length_field = Array.from(Array(fields.length).keys());
+    // var records=this._getExportDialogWidget().record.res_ids
+    // var data_len=Array.from(Array(records.length).keys());
+    // var group_length=Array.from(Array(self.renderer.state.groupsCount).keys());
+    // ajax.jsonRpc('/get_data','call',{
+    // 'field':fields,
+    // 'model':this._getExportDialogWidget().record.model,
+    // 'res_ids':records,
+    // 'exported_fields':exported_fields,
+    // 'grouped_by':groupedBy,
+    // 'grouped_by_ids': this.initialState.res_ids,
+    // 'amount': this.renderer.state.data.map(({aggregateValues}) =>aggregateValues),
+    // 'domain':this._getExportDialogWidget().record.domain
+    // })
+    // .then( function (data){
+    //     if (groupedBy[0]){
+
+    //         var action = {
+    //                     'type': 'ir.actions.report',
+    //                     'report_type': 'qweb-pdf',
+    //                     'report_name':'unpaid_invoice.export_in_pdf_group_by',
+    //                     'data':{'length':length_field,'group_len':group_length,'record':data,}
+    //                 };
+    //     }
+    //     else{
+    //         var action = {
+    //                 'type': 'ir.actions.report',
+    //                 'report_type': 'qweb-pdf',
+    //                 'report_name':'unpaid_invoice.export_in_pdf',
+    //                 'data':{'length':length_field,'data_len':data_len,'record':data}
+    //             };
+    //     }
+    //     return self.do_action(action);
+    //     });
+    //    }
     action_def: function () {
-    var self=this;
-    var fields=this._getExportDialogWidget().defaultExportFields;
-    let exported_fields = this._getExportDialogWidget().defaultExportFields.map(field => ({
+        var self = this;
+        var fields = this._getExportDialogWidget().defaultExportFields;
+        let exported_fields = fields.map(field => ({
             name: field,
             label: this._getExportDialogWidget().record.fields[field].string,
         }));
-    let groupedBy = this.renderer.state.groupedBy;
-    var length_field = Array.from(Array(fields.length).keys());
-    var records=this._getExportDialogWidget().record.res_ids
-    var data_len=Array.from(Array(records.length).keys());
-    var group_length=Array.from(Array(self.renderer.state.groupsCount).keys());
-    ajax.jsonRpc('/get_data','call',{
-    'field':fields,
-    'model':this._getExportDialogWidget().record.model,
-    'res_ids':records,
-    'exported_fields':exported_fields,
-    'grouped_by':groupedBy,
-    'grouped_by_ids': this.initialState.res_ids,
-    'amount': this.renderer.state.data.map(({aggregateValues}) =>aggregateValues),
-    'domain':this._getExportDialogWidget().record.domain
-    })
-    .then( function (data){
-        if (groupedBy[0]){
-
-            var action = {
-                        'type': 'ir.actions.report',
-                        'report_type': 'qweb-pdf',
-                        'report_name':'unpaid_invoice.export_in_pdf_group_by',
-                        'data':{'length':length_field,'group_len':group_length,'record':data,}
-                    };
-        }
-        else{
-            var action = {
+        let groupedBy = this.renderer.state.groupedBy;
+        var records = this._getExportDialogWidget().record.res_ids;
+    
+        // Step 1: Generate the PDF report
+        ajax.jsonRpc('/get_data', 'call', {
+            'field': fields,
+            'model': this._getExportDialogWidget().record.model,
+            'res_ids': records,
+            'exported_fields': exported_fields,
+            'grouped_by': groupedBy,
+            'domain': this._getExportDialogWidget().record.domain
+        }).then(function (data) {
+            var action;
+            if (groupedBy[0]) {
+                action = {
                     'type': 'ir.actions.report',
                     'report_type': 'qweb-pdf',
-                    'report_name':'unpaid_invoice.export_in_pdf',
-                    'data':{'length':length_field,'data_len':data_len,'record':data}
+                    'report_name': 'unpaid_invoice.export_in_pdf_group_by',
+                    'data': {'record': data}
                 };
-        }
-        return self.do_action(action);
+            } else {
+                action = {
+                    'type': 'ir.actions.report',
+                    'report_type': 'qweb-pdf',
+                    'report_name': 'unpaid_invoice.export_in_pdf',
+                    'data': {'record': data}
+                };
+            }
+    
+            // Execute the report action
+            return self.do_action(action);
+        }).then(function () {
+            // Step 2: Send email with the PDF attachment
+            return ajax.jsonRpc('/send_email_unpaid_invoices', 'call', {
+                'template_id': 'unpaid_invoice.unpaid_invoice',
+                'report_name': groupedBy[0] ? 'unpaid_invoice.export_in_pdf_group_by' : 'unpaid_invoice.export_in_pdf',
+                'records': records
+            });
+        }).then(function (email_response) {
+            if (email_response.success) {
+                console.log('Email sent successfully.');
+            } else {
+                console.error('Failed to send email:', email_response.error);
+            }
         });
-       }
+    }
+    
+
    });
    });
