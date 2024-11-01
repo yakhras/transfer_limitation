552bd12aa7a0f61348e9ad28ca0d43b179f93270
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-11-01 09:44:55 +0300

    edt

--- unpaid_invoice/static/src/js/pdf_export.js
+++ unpaid_invoice/static/src/js/pdf_export.js
@@ -1,132 +1,72 @@
-odoo.define('unpaid_invoice.print_pdf',function(require){
+odoo.define('unpaid_invoice.print_pdf', function (require) {
     "use strict";
-var config = require('web.config');
-var core = require('web.core');
-var ListController = require('web.ListController');
-var ajax = require('web.ajax');
-ListController.include({
-   renderButtons: function($node) {
-   this._super.apply(this, arguments);
-       if (this.$buttons) {
-         this.$buttons.find('.o_list_export_pdf').click(this.proxy('action_def')) ;
-       }
-   },
-    // action_def: function () {
-    // var self=this;
-    // var fields=this._getExportDialogWidget().defaultExportFields;
-    // let exported_fields = this._getExportDialogWidget().defaultExportFields.map(field => ({
-    //         name: field,
-    //         label: this._getExportDialogWidget().record.fields[field].string,
-    //     }));
-    // let groupedBy = this.renderer.state.groupedBy;
-    // var length_field = Array.from(Array(fields.length).keys());
-    // var records=this._getExportDialogWidget().record.res_ids
-    // var data_len=Array.from(Array(records.length).keys());
-    // var group_length=Array.from(Array(self.renderer.state.groupsCount).keys());
-    // ajax.jsonRpc('/get_data','call',{
-    // 'field':fields,
-    // 'model':this._getExportDialogWidget().record.model,
-    // 'res_ids':records,
-    // 'exported_fields':exported_fields,
-    // 'grouped_by':groupedBy,
-    // 'grouped_by_ids': this.initialState.res_ids,
-    // 'amount': this.renderer.state.data.map(({aggregateValues}) =>aggregateValues),
-    // 'domain':this._getExportDialogWidget().record.domain
-    // })
-    // .then( function (data){
-    //     if (groupedBy[0]){
-
-    //         var action = {
-    //                     'type': 'ir.actions.report',
-    //                     'report_type': 'qweb-pdf',
-    //                     'report_name':'unpaid_invoice.export_in_pdf_group_by',
-    //                     'data':{'length':length_field,'group_len':group_length,'record':data,}
-    //                 };
-    //     }
-    //     else{
-    //         var action = {
-    //                 'type': 'ir.actions.report',
-    //                 'report_type': 'qweb-pdf',
-    //                 'report_name':'unpaid_invoice.export_in_pdf',
-    //                 'data':{'length':length_field,'data_len':data_len,'record':data}
-    //             };
-    //     }
-    //     return self.do_action(action);
-    //     });
-    //    }
-
-
-    action_def: function () {
-        var self = this;
-        var fields = this._getExportDialogWidget().defaultExportFields;
-        let exported_fields = this._getExportDialogWidget().defaultExportFields.map(field => ({
-            name: field,
-            label: this._getExportDialogWidget().record.fields[field].string,
-        }));
-        let groupedBy = this.renderer.state.groupedBy;
-        var length_field = Array.from(Array(fields.length).keys());
-        var records = this._getExportDialogWidget().record.res_ids;
-        var data_len = Array.from(Array(records.length).keys());
-        var group_length = Array.from(Array(self.renderer.state.groupsCount).keys());
-    
-        // Prepare the data for PDF generation
-        ajax.jsonRpc('/get_data', 'call', {
-            'field': fields,
-            'model': this._getExportDialogWidget().record.model,
-            'res_ids': records,
-            'exported_fields': exported_fields,
-            'grouped_by': groupedBy,
-            'grouped_by_ids': this.initialState.res_ids,
-            'amount': this.renderer.state.data.map(({aggregateValues}) => aggregateValues),
-            'domain': this._getExportDialogWidget().record.domain
-        })
-        .then(function (data) {
-            var action;
-    
-            if (groupedBy[0]) {
-                action = {
-                    'type': 'ir.actions.report',
-                    'report_type': 'qweb-pdf',
-                    'report_name': 'unpaid_invoice.export_in_pdf_group_by',
-                    'data': {'length': length_field, 'group_len': group_length, 'record': data}
-                };
-            } else {
-                action = {
-                    'type': 'ir.actions.report',
-                    'report_type': 'qweb-pdf',
-                    'report_name': 'unpaid_invoice.export_in_pdf',
-                    'data': {'length': length_field, 'data_len': data_len, 'record': data}
-                };
+    var config = require('web.config');
+    var core = require('web.core');
+    var ListController = require('web.ListController');
+    var ajax = require('web.ajax');
+    ListController.include({
+        renderButtons: function ($node) {
+            this._super.apply(this, arguments);
+            if (this.$buttons) {
+                this.$buttons.find('.o_list_export_pdf').click(this.proxy('action_def'));
             }
-    
-            // Generate PDF and send to the server
-            return self.do_action(action).then(function (report) {
-                console.log(report.pdf)
-                // After the PDF is generated, send it to the server
-                // if (report && report.pdf) {
-                //     // Send the PDF data to the server to send an email
-                //     return self._rpc({
-                //         route: '/your_endpoint_to_send_pdf',
-                //         params: {
-                //             pdf_data: report.pdf, // PDF binary content
-                //             // Add any other parameters if necessary
-                //         },
-                //     }).then(function (response) {
-                //         // Logging the response
-                //         console.log("Response from server:", response);
-    
-                //         if (response.success) {
-                //             alert("PDF sent successfully!");
-                //         } else {
-                //             alert("Failed to send PDF: " + response.error);
-                //         }
-                //     })
-                // }
-            });
-        });
-    }
+        },
+        action_def: function () {
+            var self = this;
+            var fields = this._getExportDialogWidget().defaultExportFields;
+            let exported_fields = this._getExportDialogWidget().defaultExportFields.map(field => ({
+                name: field,
+                label: this._getExportDialogWidget().record.fields[field].string,
+            }));
+            let groupedBy = this.renderer.state.groupedBy;
+            var length_field = Array.from(Array(fields.length).keys());
+            var records = this._getExportDialogWidget().record.res_ids
+            var data_len = Array.from(Array(records.length).keys());
+            var group_length = Array.from(Array(self.renderer.state.groupsCount).keys());
+            ajax.jsonRpc('/get_data', 'call', {
+                'field': fields,
+                'model': this._getExportDialogWidget().record.model,
+                'res_ids': records,
+                'exported_fields': exported_fields,
+                'grouped_by': groupedBy,
+                'grouped_by_ids': this.initialState.res_ids,
+                'amount': this.renderer.state.data.map(({ aggregateValues }) => aggregateValues),
+                'domain': this._getExportDialogWidget().record.domain
+            })
+                .then(function (data) {
+                    if (groupedBy[0]) {
 
-    
+                        var action = {
+                            'type': 'ir.actions.report',
+                            'report_type': 'qweb-pdf',
+                            'report_name': 'unpaid_invoice.export_in_pdf_group_by',
+                            'data': { 'length': length_field, 'group_len': group_length, 'record': data, }
+                        };
+                    }
+                    else {
+                        var action = {
+                            'type': 'ir.actions.report',
+                            'report_type': 'qweb-pdf',
+                            'report_name': 'unpaid_invoice.export_in_pdf',
+                            'data': { 'length': length_field, 'data_len': data_len, 'record': data }
+                        };
+                    }
+                    return self.do_action(action);
+                });
+        }
 
-   });
-   });
+    });
+});
+
+// static/src/js/console_printer.js
+odoo.define('unpaid_invoice.console_printer', function (require) {
+    "use strict";
+
+    // This ensures your code only runs once the Odoo web client is ready
+    const { Component } = require('web.OwlMixin');
+
+    // Print a message on the console every 1 minute (60000 milliseconds)
+    setInterval(() => {
+        console.log("Hello! This message appears every 1 minute.");
+    }, 60000);
+});
