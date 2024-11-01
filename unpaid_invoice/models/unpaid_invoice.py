430695ee69f678bea79e10e0b80aed2b12589584
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-11-01 14:39:38 +0300

    edt

--- unpaid_invoice/models/unpaid_invoice.py
+++ unpaid_invoice/models/unpaid_invoice.py
@@ -33,8 +33,6 @@ class UnpaidInvoice(models.Model):
     amount_total = fields.Monetary(string="Total Amount", currency_field='currency_id')
     currency_id = fields.Many2one('res.currency', string="Currency")
     unpaid_invoice_count = fields.Char(string="Unpaid Invoice Count",compute='_compute_details')
-    action_id = fields.Integer(string='Action ID', compute='_compute_details')
-    action_domain = fields.Char(string='Action Domain', compute='_compute_details')
 
 
     def populate_unpaid_invoices(self):
