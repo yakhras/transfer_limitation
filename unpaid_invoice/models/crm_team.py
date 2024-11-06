3476014f9fca2e5e62dec24dd1fdd4e043ae305d
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-11-06 18:23:40 +0300

    edt

--- unpaid_invoice/models/crm_team.py
+++ unpaid_invoice/models/crm_team.py
@@ -23,6 +23,13 @@ class CrmTeam(models.Model):
         string='Total Due in EUR for Last 2 Weeks', compute='_compute_unpaid_invoice_totals_2weeks')
     unpaid_invoice_total_2weeks_try = fields.Float(
         string='Total Due in TRY for Last 2 Weeks', compute='_compute_unpaid_invoice_totals_2weeks')
+    
+    unpaid_invoice_total_week_usd = fields.Float(
+        string='Total Due in USD for Last Week', compute='_compute_unpaid_invoice_totals_week')
+    unpaid_invoice_total_week_eur = fields.Float(
+        string='Total Due in EUR for Last Week', compute='_compute_unpaid_invoice_totals_week')
+    unpaid_invoice_total_week_try = fields.Float(
+        string='Total Due in TRY for Last Week', compute='_compute_unpaid_invoice_totals_week')
 
     # Currency fields for multi-currency support
     currency_usd = fields.Many2one('res.currency', default=lambda self: self.env.ref('base.USD').id, readonly=True)
@@ -89,3 +96,33 @@ class CrmTeam(models.Model):
             team.unpaid_invoice_total_2weeks_usd = total_usd
             team.unpaid_invoice_total_2weeks_eur = total_eur
             team.unpaid_invoice_total_2weeks_try = total_try
+
+
+    def _compute_unpaid_invoice_totals_week(self):
+        today = fields.Date.today()
+        one_week_ago = (today  + date_utils.relativedelta(weeks=-1)).strftime('%Y-%m-%d')  # Calculate the date 7 days ago
+
+        for team in self:
+            total_usd = total_eur = total_try = 0.0  # Initialize totals for each currency
+            
+            domain = [
+                ('due_date', '>=', one_week_ago),
+                ('due_date', '<', today),
+                ('team_id', '=', team.id),
+            ]
+            invoices = self.env['unpaid.invoice'].search(domain)
+
+            # Sum amounts by currency
+            for invoice in invoices:
+                if invoice.currency_id == team.currency_usd:
+                    total_usd += invoice.amount_due
+                elif invoice.currency_id == team.currency_eur:
+                    total_eur += invoice.amount_due
+                elif invoice.currency_id == team.currency_try:
+                    total_try += invoice.amount_due
+
+
+            # Assign totals to the respective fields
+            team.unpaid_invoice_total_week_usd = total_usd
+            team.unpaid_invoice_total_week_eur = total_eur
+            team.unpaid_invoice_total_week_try = total_try
