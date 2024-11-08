dfc16d2d150e0122ba0e95f6aed6cea772ebc7ce
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-11-08 15:21:13 +0300

    edt

--- unpaid_invoice/models/crm_team.py
+++ unpaid_invoice/models/crm_team.py
@@ -1,6 +1,6 @@
 from odoo.tools import date_utils
 from odoo import models, fields, api
-from datetime import datetime
+from datetime import date
 
 class CrmTeam(models.Model):
     _inherit = 'crm.team'
@@ -51,7 +51,7 @@ class CrmTeam(models.Model):
 
     def _get_date_range(self, weeks=0):
         """Helper method to calculate date range."""
-        today = datetime.today()  # Keep it as a date object
+        today = date.today()  # Keep it as a date object
         start_date = today + date_utils.relativedelta(weeks=weeks)  # Apply relativedelta
         return start_date, today  # Convert both to string in the correct format
 
