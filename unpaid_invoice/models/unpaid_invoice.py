bbc182fb96d8bf27b3e72b120ecaf89cd5d92f6b
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-11-21 17:11:54 +0300

    edt

--- unpaid_invoice/models/unpaid_invoice.py
+++ unpaid_invoice/models/unpaid_invoice.py
@@ -70,6 +70,7 @@ class UnpaidInvoice(models.Model):
             ('state', '=', 'posted'),
             ('move_type', 'in', ['out_invoice', 'out_refund']),
             ('payment_state', 'in', ['not_paid', 'partial']),
+            ('amount_residual_signed',"!=",0),
         ]
 
         # Fetch records from account.move that meet the domain criteria
