80306673f7645e6f7b1fd00213858d8f1cf1431e
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-11-25 18:21:33 +0300

    edt

--- proforma_custom/models/sale_order.py
+++ proforma_custom/models/sale_order.py
@@ -8,8 +8,23 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'   # Inherit the model
 
     has_note = fields.Boolean(string="Has Note", compute="_compute_has_note")
+    expiration_days = fields.Char(
+        string="Expiration Days",
+        compute="_compute_expiration_days",
+        store=False  # No need to store this, as it's dynamic and only used in the report
+    )
+
 
     def _compute_has_note(self):
         for record in self:
             # Check if `note` has meaningful content (ignoring whitespace and empty HTML tags)
             record.has_note = bool(record.note)
+
+
+    def _compute_expiration_days(self):
+        for order in self:
+            if order.validity_date and order.date_order:
+                delta = order.validity_date - order.date_order
+                order.expiration_days = f"{delta.days} Days"
+            else:
+                order.expiration_days = "N/A"
