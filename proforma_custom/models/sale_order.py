80f7eb032d2e4359383e82ce0ca5e18ff16a775f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-11-29 09:16:30 +0300

    edt

--- proforma_custom/models/sale_order.py
+++ proforma_custom/models/sale_order.py
@@ -32,5 +32,11 @@ class SaleOrder(models.Model):
                 order.expiration_days = "N/A"
 
 
-    def get_partner_name_title_case(self):
-        return self.partner_id.name.title() if self.partner_id.name else ''
+    partner_name_title_case = fields.Char(
+        compute='_compute_partner_name_title_case', 
+        string='Partner Name (Title Case)'
+    )
+
+    def _compute_partner_name_title_case(self):
+        for record in self:
+            record.partner_name_title_case = record.partner_id.name.title() if record.partner_id.name else ''
