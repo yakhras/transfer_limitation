8decfb45119a23c420d89f3ce559f46648d1d956
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-12-04 14:46:56 +0300

    edt

--- unpaid_invoice/models/month_record.py
+++ unpaid_invoice/models/month_record.py
@@ -28,27 +28,27 @@ class MonthRecord(models.Model):
 
     def _compute_totals(self):
         today = date.today()
-        week_start = today - timedelta(days=today.weekday() + 1)  # Last Saturday
-        week_end = week_start + timedelta(days=6)  # Next Friday
-        month_start = today.replace(day=1)
-        month_end = (month_start + timedelta(days=31)).replace(day=1) - timedelta(days=1)
+        # week_start = today - timedelta(days=today.weekday() + 1)  # Last Saturday
+        # week_end = week_start + timedelta(days=6)  # Next Friday
+        # month_start = today.replace(day=1)
+        # month_end = (month_start + timedelta(days=31)).replace(day=1) - timedelta(days=1)
 
         for record in self:
             record.today_immediate = self._calculate_total(today, today, 'Immediate')
             record.today_transfer = self._calculate_total(today, today, 'Transfer')
             record.today_check = self._calculate_total(today, today, 'Check')
 
-            record.this_week_immediate = self._calculate_total(week_start, week_end, 'Immediate')
-            record.this_week_transfer = self._calculate_total(week_start, week_end, 'Transfer')
-            record.this_week_check = self._calculate_total(week_start, week_end, 'Check')
+            # record.this_week_immediate = self._calculate_total(week_start, week_end, 'Immediate')
+            # record.this_week_transfer = self._calculate_total(week_start, week_end, 'Transfer')
+            # record.this_week_check = self._calculate_total(week_start, week_end, 'Check')
 
-            record.this_month_immediate = self._calculate_total(month_start, month_end, 'Immediate')
-            record.this_month_transfer = self._calculate_total(month_start, month_end, 'Transfer')
-            record.this_month_check = self._calculate_total(month_start, month_end, 'Check')
+            # record.this_month_immediate = self._calculate_total(month_start, month_end, 'Immediate')
+            # record.this_month_transfer = self._calculate_total(month_start, month_end, 'Transfer')
+            # record.this_month_check = self._calculate_total(month_start, month_end, 'Check')
 
-            record.other_immediate = self._calculate_total(month_end + timedelta(days=1), None, 'Immediate')
-            record.other_transfer = self._calculate_total(month_end + timedelta(days=1), None, 'Transfer')
-            record.other_check = self._calculate_total(month_end + timedelta(days=1), None, 'Check')
+            # record.other_immediate = self._calculate_total(month_end + timedelta(days=1), None, 'Immediate')
+            # record.other_transfer = self._calculate_total(month_end + timedelta(days=1), None, 'Transfer')
+            # record.other_check = self._calculate_total(month_end + timedelta(days=1), None, 'Check')
 
 
     def _calculate_total(self, start_date, end_date, term):
