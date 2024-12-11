b5f309a894b1c6d1d2761b06cb7ed03276ed792a
yaserakhras <58257996+yakhras@users.noreply.github.com>
2024-12-11 16:52:21 +0300

    edt

--- unpaid_invoice/models/account_move.py
+++ unpaid_invoice/models/account_move.py
@@ -1,86 +1,117 @@
-from odoo import models, fields, api
-from datetime import date
+from odoo import models, fields, api, _
+from datetime import date, timedelta
 import logging
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    _logger = logging.getLogger(__name__)
+    # _logger = logging.getLogger(__name__)
 
-    is_due_and_unpaid = fields.Boolean(
-        compute='_compute_due_and_unpaid',
-        store=True
-    )
+    # is_due_and_unpaid = fields.Boolean(
+    #     compute='_compute_due_and_unpaid',
+    #     store=True
+    # )
 
-    @api.depends('invoice_date_due', 'payment_state')
-    def _compute_due_and_unpaid(self):
-        unpaid_invoice_model = self.env['unpaid.invoice']
-        for move in self:
-            # Check if due today and unpaid or partially paid
-            move.is_due_and_unpaid = (
-                move.invoice_date_due == date.today() and
-                move.payment_state in ['not_paid', 'partial'] and
-                move.move_type in ['out_invoice', 'out_refund']
-            )
+    # @api.depends('invoice_date_due', 'payment_state')
+    # def _compute_due_and_unpaid(self):
+    #     unpaid_invoice_model = self.env['unpaid.invoice']
+    #     for move in self:
+    #         # Check if due today and unpaid or partially paid
+    #         move.is_due_and_unpaid = (
+    #             move.invoice_date_due == date.today() and
+    #             move.payment_state in ['not_paid', 'partial'] and
+    #             move.move_type in ['out_invoice', 'out_refund']
+    #         )
 
-            # Trigger creation in unpaid.invoice if condition met
-            if move.is_due_and_unpaid:
-                existing_unpaid_invoice = unpaid_invoice_model.search([('invoice_id', '=', move.id)], limit=1)
-                if not existing_unpaid_invoice:
-                    unpaid_invoice_model.create({
-                        'invoice_id': move.id,
-                        'amount_due': move.amount_residual,
-                        'due_date': move.invoice_date_due,
-                        'partner_id': move.partner_id.id,
-                        'team_id': move.team_id.id,
-                        'currency_id': move.currency_id.id,
-                    })
+    #         # Trigger creation in unpaid.invoice if condition met
+    #         if move.is_due_and_unpaid:
+    #             existing_unpaid_invoice = unpaid_invoice_model.search([('invoice_id', '=', move.id)], limit=1)
+    #             if not existing_unpaid_invoice:
+    #                 unpaid_invoice_model.create({
+    #                     'invoice_id': move.id,
+    #                     'amount_due': move.amount_residual,
+    #                     'due_date': move.invoice_date_due,
+    #                     'partner_id': move.partner_id.id,
+    #                     'team_id': move.team_id.id,
+    #                     'currency_id': move.currency_id.id,
+    #                 })
 
 
 
-    def send_due_invoices_email(self):
-        """Send an email template without linking it to a specific record."""
-        try:
-            # Define the email template
-            template_id = self.env.ref('unpaid_invoice.unpaid_invoice', raise_if_not_found=False)
-            if not template_id:
-                self._logger.error("Email template 'email_template_due_invoices' not found.")
-                return
+    # def send_due_invoices_email(self):
+    #     """Send an email template without linking it to a specific record."""
+    #     try:
+    #         # Define the email template
+    #         template_id = self.env.ref('unpaid_invoice.unpaid_invoice', raise_if_not_found=False)
+    #         if not template_id:
+    #             self._logger.error("Email template 'email_template_due_invoices' not found.")
+    #             return
             
-            # Use the template's model, or set a dummy model if needed
-            # Set context to bypass requiring a specific record
-            mail = self.env['mail.mail'].create({
-                'subject': template_id.subject,
-                'body_html': template_id.body_html,
-                'email_to': 'yaser.akhras@menagate.com.tr',  # Replace with your desired email address
-                'email_from': 'yaser.akhras@menagate.com.tr'
-            })
-            mail.send()
-            self._logger.info("Email sent successfully.")
-        except Exception as e:
-            self._logger.error(f"Failed to send email: {e}")
+    #         # Use the template's model, or set a dummy model if needed
+    #         # Set context to bypass requiring a specific record
+    #         mail = self.env['mail.mail'].create({
+    #             'subject': template_id.subject,
+    #             'body_html': template_id.body_html,
+    #             'email_to': 'yaser.akhras@menagate.com.tr',  # Replace with your desired email address
+    #             'email_from': 'yaser.akhras@menagate.com.tr'
+    #         })
+    #         mail.send()
+    #         self._logger.info("Email sent successfully.")
+    #     except Exception as e:
+    #         self._logger.error(f"Failed to send email: {e}")
 
 
-    total_residual_signed_december = fields.Monetary(
-        string="Total Residual (December)",
-        compute="_compute_total_residual_signed_december",
-        currency_field='currency_id',
-        store=False  # Set to True if you want it stored in the database
-    )
+    # total_residual_signed_december = fields.Monetary(
+    #     string="Total Residual (December)",
+    #     compute="_compute_total_residual_signed_december",
+    #     currency_field='currency_id',
+    #     store=False  # Set to True if you want it stored in the database
+    # )
 
-    @api.depends()
-    def _compute_total_residual_signed_december(self):
-        """Compute the total amount_residual_signed for December."""
-        domain = [
-            ('invoice_date_due', '>=', '2024-12-01'),
-            ('invoice_date_due', '<=', '2024-12-31'),
-            ('state', '=', 'posted'),
-            ('move_type', 'in', ['out_invoice', 'out_refund']),
-            ('payment_state', 'in', ['not_paid', 'partial']),
-            ('line_ids.account_id.code', '=', '120001'),
-            ('amount_residual_signed', '!=', 0),
-        ]
-        for record in self:
-            invoices = self.search(domain)
-            record.total_residual_signed_december = sum(invoices.mapped('amount_residual_signed'))
+    # @api.depends()
+    # def _compute_total_residual_signed_december(self):
+    #     """Compute the total amount_residual_signed for December."""
+    #     domain = [
+    #         ('invoice_date_due', '>=', '2024-12-01'),
+    #         ('invoice_date_due', '<=', '2024-12-31'),
+    #         ('state', '=', 'posted'),
+    #         ('move_type', 'in', ['out_invoice', 'out_refund']),
+    #         ('payment_state', 'in', ['not_paid', 'partial']),
+    #         ('line_ids.account_id.code', '=', '120001'),
+    #         ('amount_residual_signed', '!=', 0),
+    #     ]
+    #     for record in self:
+    #         invoices = self.search(domain)
+    #         record.total_residual_signed_december = sum(invoices.mapped('amount_residual_signed'))
+
+    def action_saturday_to_friday(self):
+        today = date.today()
+        week_start = today - timedelta(days=today.weekday() + 1)  # Previous Saturday
+        week_end = week_start + timedelta(days=6)  # Following Friday
+
+        action = {
+            "name": _("Unpaid Invoice November"),
+            "type": "ir.actions.act_window",
+            "res_model": "account.move",
+            "view_mode": "tree,form",
+            "view_id": self.env.ref("account_move.view_account_move_custom_list").id,
+            "target": "current",
+            "domain":[
+                ('invoice_date_due', '>=', week_start.strftime('%Y-%m-%d')),
+                ('invoice_date_due', '<=', week_end.strftime('%Y-%m-%d')),
+                ('state', '=', 'posted'),
+                ('move_type', 'in', ['out_invoice', 'out_refund']),
+                ('payment_state', 'in', ['not_paid', 'partial']),
+                ('line_ids.account_id.code',"=",120001),
+                ('amount_residual_signed',"!=",0),
+            ],
+            "context": {
+                    'default_move_type':'out_invoice',
+                    'move_type':'out_invoice',
+                    'journal_type': 'sale',
+                    'group_by': ['invoice_user_id','partner_id'],
+                },
+            "search_view_id": self.env.ref("account.view_out_invoice_tree").id,
+        }
+        return action
 
