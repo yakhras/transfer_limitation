c6dd58d063aadbf81f20fe7f5ea7a54f0bdc9bd6
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-01-14 11:45:35 +0300

    edt

--- partner_access/models/res_users.py
+++ partner_access/models/res_users.py
@@ -6,21 +6,28 @@ class Users(models.Model):
     def get_partner_ids(self):
         partner_ids = set()
 
+        # Fetch the current user using self.env.user
+        current_user = self.env.user
+
         # Add the bot user's partner ID
         bot_user = self.env['res.users'].sudo().browse(1)
         if not bot_user.active and bot_user.partner_id:
             partner_ids.add(bot_user.partner_id.id)
 
-        # Add partner IDs for users managed by the current user
-        manager_users = self.env['hr.employee'].search([('parent_id.user_id', '=', self.env.user.id)]).mapped('user_id.partner_id.id')
-        partner_ids.update(manager_users)
+        # Fetch employees managed by the current user
+        employees = self.env['hr.employee'].search([('parent_id.user_id', '=', current_user.id)])
+        manager_users = employees.mapped('user_id')
+
+        # Retrieve partner IDs of the managed employees
+        direct_partners = self.env['res.partner'].search([('user_id', 'in', manager_users.ids)])
+        partner_ids.update(direct_partners.ids)
 
         # Add partner IDs of active internal users
-        # internal_users = self.env['res.users'].search([
-        #     ('active', '=', True),
-        #     ('groups_id', 'in', self.env.ref('base.group_user').id)
-        # ]).mapped('partner_id.id')
-        # partner_ids.update(internal_users)
+        internal_users = self.env['res.users'].search([
+            ('active', '=', True),
+            ('groups_id', 'in', self.env.ref('base.group_user').id)
+        ]).mapped('partner_id.id')
+        partner_ids.update(internal_users)
 
         # Convert the set to a list
         partner_ids = list(partner_ids)
