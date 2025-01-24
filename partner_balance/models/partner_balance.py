e9b19e95a58ca5e803b237a495e222798001ddb5
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-01-24 12:42:36 +0300

    edt

--- partner_balance/models/partner_balance.py
+++ partner_balance/models/partner_balance.py
@@ -12,6 +12,7 @@ class PartnerBalance(models.Model):
         required=True,
         ondelete='cascade'
     )
+    sales_user = fields.Many2one(related= 'partner_id.user_id', string= 'Sales Person')
     move_line_ids = fields.One2many(
         'account.move.line', 
         compute='_compute_move_lines',
