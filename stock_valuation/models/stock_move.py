c0509cd44239c8da08539f47da6da8e8f2b90463
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-02-20 14:56:45 +0300

    edt

--- stock_valuation/models/stock_move.py
+++ stock_valuation/models/stock_move.py
@@ -13,6 +13,22 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
 
+    def _get_in_move_lines(self, location):
+        """ Returns the `stock.move.line` records of `self` considered as incoming. It is done thanks
+        to the `_should_be_valued` method of their source and destionation location as well as their
+        owner.
+
+        :returns: a subset of `self` containing the incoming records
+        :rtype: recordset
+        """
+        self.ensure_one()
+        res = OrderedSet()
+        for move_line in self.move_line_ids.filtered(lambda ml: ml.location_id == location):
+            if move_line.owner_id and move_line.owner_id != move_line.company_id.partner_id:
+                continue
+            if not move_line.location_id._should_be_valued() and move_line.location_dest_id._should_be_valued():
+                res.add(move_line.id)
+        return self.env['stock.move.line'].browse(res)
 
     def _get_out_move_lines(self, location):
         """ Returns the `stock.move.line` records of `self` considered as outgoing. It is done thanks
@@ -30,7 +46,29 @@ class StockMove(models.Model):
                 res |= move_line
         return res
     
+    def _create_in_svl(self, forced_quantity=None):
+        """Create a `stock.valuation.layer` from `self`.
 
+        :param forced_quantity: under some circunstances, the quantity to value is different than
+            the initial demand of the move (Default value = None)
+        """
+        svl_vals_list = []
+        for move in self:
+            move = move.with_company(move.company_id)
+            valued_move_lines = move._get_in_move_lines(move.location_id)
+            valued_quantity = 0
+            for valued_move_line in valued_move_lines:
+                valued_quantity += valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, move.product_id.uom_id)
+            unit_cost = abs(move._get_price_unit())  # May be negative (i.e. decrease an out move).
+            if move.product_id.cost_method == 'standard':
+                unit_cost = move.product_id.standard_price
+            svl_vals = move.product_id._prepare_in_svl_vals(forced_quantity or valued_quantity, unit_cost)
+            svl_vals.update(move._prepare_common_svl_vals())
+            if forced_quantity:
+                svl_vals['description'] = 'Correction of %s (modification of past move)' % move.picking_id.name or move.name
+            svl_vals_list.append(svl_vals)
+        return self.env['stock.valuation.layer'].sudo().create(svl_vals_list)
+    
     def _create_out_svl(self, forced_quantity=None):
         """Create a `stock.valuation.layer` from `self`.
 
@@ -54,7 +92,18 @@ class StockMove(models.Model):
             svl_vals_list.append(svl_vals)
         return self.env['stock.valuation.layer'].sudo().create(svl_vals_list)
     
+    def _is_in(self):
+        """Check if the move should be considered as entering the company so that the cost method
+        will be able to apply the correct logic.
 
+        :returns: True if the move is entering the company else False
+        :rtype: bool
+        """
+        self.ensure_one()
+        if self._get_in_move_lines(self.location_id) and not self._is_dropshipped_returned():
+            return True
+        return False
+    
     def _is_out(self):
         """Check if the move should be considered as leaving the company so that the cost method
         will be able to apply the correct logic.
@@ -66,3 +115,8 @@ class StockMove(models.Model):
         if self._get_out_move_lines(self.location_id) and not self._is_dropshipped():
             return True
         return False
+    
+    
+    
+
+    
