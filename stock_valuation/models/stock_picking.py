080033f9384720963a318d4a875fc619837c5db0
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-02-25 15:35:07 +0300

    edt

--- stock_valuation/models/stock_picking.py
+++ stock_valuation/models/stock_picking.py
@@ -16,30 +16,15 @@ class Picking(models.Model):
 
 
 
-    def _action_done(self):
-            """Call `_action_done` on the `stock.move` of the `stock.picking` in `self`.
-            This method makes sure every `stock.move.line` is linked to a `stock.move` by either
-            linking them to an existing one or a newly created one.
-
-            If the context key `cancel_backorder` is present, backorders won't be created.
-
-            :return: True
-            :rtype: bool
-            """
-            self._check_company()
-
-            todo_moves = self.mapped('move_lines').filtered(lambda self: self.state in ['draft', 'waiting', 'partially_available', 'assigned', 'confirmed'])
-            for picking in self:
-                if picking.owner_id:
-                    picking.move_lines.write({'restrict_partner_id': picking.owner_id.id})
-                    picking.move_line_ids.write({'owner_id': picking.owner_id.id})
-            location = self.location_dest_id
-            todo_moves._action_done(location, cancel_backorder=self.env.context.get('cancel_backorder'))
-            self.write({'date_done': fields.Datetime.now(), 'priority': '0'})
-
-            # if incoming moves make other confirmed/partially_available moves available, assign them
-            done_incoming_moves = self.filtered(lambda p: p.picking_type_id.code == 'incoming').move_lines.filtered(lambda m: m.state == 'done')
-            done_incoming_moves._trigger_assign()
-
-            self._send_confirmation_email()
-            return True
+    def _action_generate_immediate_wizard(self, show_transfers=False):
+        view = self.env.ref('stock.view_immediate_transfer')
+        return {
+            'name': _('Immediate Transfer?'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'stock.immediate.transfer',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'context': dict(self.env.context, default_show_transfers=show_transfers, default_pick_ids=[(4, p.id) for p in self], location_dest_id=self.location_dest_id),
+        }
