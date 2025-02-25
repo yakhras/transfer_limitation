c9b36b69328acd06284544f2a239af0ff4b95bec
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-02-25 11:16:41 +0300

    edt

--- stock_valuation/models/product_product.py
+++ stock_valuation/models/product_product.py
@@ -75,7 +75,7 @@ class ProductProduct(models.Model):
             to_date = fields.Datetime.to_datetime(self.env.context['to_date'])
             domain.append(('create_date', '<=', to_date))
         groups = self.env['stock.valuation.layer'].read_group(domain, ['value:sum', 'quantity:sum'], ['product_id'], orderby='id')
-        self.result = self.env['stock.valuation.layer'].filtered(lambda x: x.stock_move_id.location_id.id == 8 or x.stock_move_id.location_dest_id.id == 8).browse()
+        self.result = self.env['stock.valuation.layer'].filtered(lambda x: x.stock_move_id.location_id.id == 8).ids
         products = self.browse()
         for group in groups:
             product = self.browse(group['product_id'][0])
