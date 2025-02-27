eabb4b47f85ab8718a1cb129262ad28f7fe739f2
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-02-27 17:46:57 +0300

    edt

--- stock_valuation/models/stock_move.py
+++ stock_valuation/models/stock_move.py
@@ -24,8 +24,9 @@ class StockMove(models.Model):
         # adapt standard price on incomming moves if the product cost_method is 'average'
         std_price_update = {}
         for move in self.filtered(lambda move: move._is_in() and move.with_company(move.company_id).product_id.cost_method == 'average'):
-            product_tot_qty_available = move.product_id.sudo().with_company(move.company_id).quantity_svl + tmpl_dict[move.product_id.id]
+            product_tot_qty_available = move.product_id.sudo().with_company(move.company_id).quantity_svl #+ tmpl_dict[move.product_id.id]
             rounding = move.product_id.uom_id.rounding
+            self.result = product_tot_qty_available
             
             
             valued_move_lines = move._get_in_move_lines()
