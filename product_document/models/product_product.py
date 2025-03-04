50556634b38677528c55175f8f43a963b21936f7
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-04 16:03:09 +0300

    edt

--- product_document/models/product_product.py
+++ product_document/models/product_product.py
@@ -10,7 +10,6 @@ class ProductInfo(models.Model):
 
 
     product_id = fields.Many2one('product.product', string='Product', required=True)
-    name = fields.Char(related='product_id.name', string='Product Name', store=True)
     default_code = fields.Char(related='product_id.default_code', string='Internal Reference', store=True)
     categ_id = fields.Many2one(related='product_id.categ_id', string='Product Category', store=True)
-    document_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'product.product')], string='Documents')
+    document_ids = fields.One2many('ir.attachment', 'res_id', domain=[('res_model', '=', 'product.product')], string='Documents', store=True)
