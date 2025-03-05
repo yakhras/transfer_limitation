30ef83d81c6862a1e206bef512f1de5ab66445ce
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-05 13:25:16 +0300

    edt

--- product_document/models/product_product.py
+++ product_document/models/product_product.py
@@ -9,9 +9,9 @@ class ProductInfo(models.Model):
 
 
 
-    product_id = fields.Many2one('product.product', string='Product', required=True)
+    product_id = fields.Many2one('product.product', string='Product', required=True, readonly=True)
     default_code = fields.Char(related='product_id.default_code', string='Internal Reference', store=True)
-    categ_id = fields.Many2one(related='product_id.categ_id', string='Product Category', store=True)
+    categ_id = fields.Many2one(related='product_id.categ_id', string='Product Category', store=True, readonly=True)
     attachment_ids = fields.Many2many('ir.attachment', 'res_id',compute='_compute_attachments')
     attachment_count = fields.Integer(string='Attachment Count', compute='_compute_attachment_count')
     attachment_urls = fields.Char(compute='_compute_attachment_urls', string="Attachment URLs")
