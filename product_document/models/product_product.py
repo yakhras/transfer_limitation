a700d0db2af74d7e1a5e87d0d7e666b8cd802030
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-06 11:00:45 +0300

    edt

--- product_document/models/product_product.py
+++ product_document/models/product_product.py
@@ -15,6 +15,21 @@ class ProductInfo(models.Model):
     categ_id = fields.Many2one(related='product_id.categ_id', string='Category', store=True, readonly=True)
     categ_name = fields.Char(related='categ_id.name', string='Product Category', store=True)
     attachment_ids = fields.Many2many('ir.attachment', 'res_id',compute='_compute_attachments')
+    available_qty_location_8 = fields.Float(string="Available Qty", compute="_compute_available_qty", store=True)
+
+    
+    def _compute_available_qty(self):
+        """ Compute available quantity from stock.quant for location 8 """
+        StockQuant = self.env['stock.quant']
+        for record in self:
+            if record.product_id:
+                quant = StockQuant.search([
+                    ('product_id', '=', record.product_id.id),
+                    ('location_id.id', '=', 8)  
+                ], limit=1)
+                record.available_qty_location_8 = quant.available_quantity if quant else 0.0
+            else:
+                record.available_qty_location_8 = 0.0
 
 
     def _compute_attachments(self):
