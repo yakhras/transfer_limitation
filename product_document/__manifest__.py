d6d2a9ed8c78126ad844a2f9d9081cbf268b267b
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-07 09:39:54 +0300

    edt

--- product_document/__manifest__.py
+++ product_document/__manifest__.py
@@ -11,10 +11,6 @@
 
     'author': "Yaser Akhras",
     #'website': "https://www.yaserakhras.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     
     'version': '15.0.1.0',
     'application': True,
@@ -27,26 +23,5 @@
     'data': [
         "security/ir.model.access.csv",
         "views/product_product_view.xml",
-        # "reports.xml", #action file.
-        # "menuitems.xml", #menu items file.
-        # "report/layouts.xml", #report page templates file. 
-        # "report/unpaid_invoice_report.xml", #report data view file
     ],
-
-    # 'assets': {
-    #     'web.assets_backend': [
-    #         'web_client/static/src/js/main.js',
-    #         'web_client/static/src/js/navbar.js',
-    #         'web_client/static/src/js/web_client.js', #buttons event function file.
-    #     ],
-    #     'web.assets_qweb': [
-    #         'web_client/static/src/xml/navbar.xml',
-    #         'web_client/static/src/xml/web_client.xml', #buttons template file.
-    #     ],
-        
-    # },
-    # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml',
-    # ],
 }
