a40166ce69d918723968d7652ff77ebb1f00efde
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-12 13:23:17 +0300

    edt

--- product_doc/models/product_info.py
+++ product_doc/models/product_info.py
@@ -14,7 +14,7 @@ class ProductInfo(models.Model):
     categ_name = fields.Char(related='categ_id.name', string='Product Category', store=True)
     attachment_ids = fields.Many2many('ir.attachment', 'res_id',compute='_compute_attachments')
     available_qty_location_8 = fields.Float(string="Available Qty", compute="_compute_available_qty", store=True)
-    weight = fields.Float(related='product_id.weight', string="Net Weight", store=True, readonly=True)
+    weight = fields.Float(related='product_id.gross_weight', string="Net Weight", store=True, readonly=True)
     gross_weight = fields.Float(string="Gross Weight", store=True, readonly=True)
     uom_name = fields.Char(related='product_id.uom_id.name', string="Unit of Measure", store=True, readonly=True)
     active = fields.Boolean(default=True)
@@ -47,10 +47,6 @@ class ProductInfo(models.Model):
                 record.attachment_ids = False
 
     
-
-
-
-    # to display the partner’s name instead of the default object name
     def name_get(self):
         result = []
         for record in self:
