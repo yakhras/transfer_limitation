c44852c8ac1cb435d23f361b48ada20dc1875735
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-12 12:53:25 +0300

    edt

--- product_doc/models/product_product.py
+++ product_doc/models/product_product.py
@@ -25,12 +25,14 @@ class ProductProduct(models.Model):
         res = super().write(vals)
         for product in self:
             if 'active' in vals:
-                info_records = self.env['product.info'].search([
-                    ('product_id', '=', product.id),
-                    '|',
-                    ('active', '=', True),
-                    ('active', '=', False)
-                    ])
+                info_records = self.env['product.info'].with_context(active_test=False).search([('product_id', '=', product.id)])
+
+                # info_records = self.env['product.info'].search([
+                #     ('product_id', '=', product.id),
+                #     '|',
+                #     ('active', '=', True),
+                #     ('active', '=', False)
+                #     ])
                 if info_records:
                     info_records.active = product.active
                     
