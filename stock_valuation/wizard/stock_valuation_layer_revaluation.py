0a40f0b671f7839b1549c2e156b953212b709cf1
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-14 15:38:31 +0300

    edt

--- stock_valuation/wizard/stock_valuation_layer_revaluation.py
+++ stock_valuation/wizard/stock_valuation_layer_revaluation.py
@@ -9,10 +9,9 @@ class StockValuationLayerRevaluation(models.TransientModel):
     _inherit = 'stock.valuation.layer.revaluation'
 
 
-    location_id = fields.Many2one('stock.location', "Related Location", required=True)
+    # location_id = fields.Many2one('stock.location', "Related Location", required=True)
 
 
     def action_validate_revaluation(self):
         self.reason = self.current_quantity_svl
         super(StockValuationLayerRevaluation, self).action_validate_revaluation()
-        
