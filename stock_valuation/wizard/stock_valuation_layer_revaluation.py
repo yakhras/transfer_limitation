ab5d0af5ec8f14ab8a6aa3159491741099c7b188
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-14 15:37:25 +0300

    edt

---
+++ stock_valuation/wizard/stock_valuation_layer_revaluation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools import float_is_zero
+
+
+class StockValuationLayerRevaluation(models.TransientModel):
+    _inherit = 'stock.valuation.layer.revaluation'
+
+
+    location_id = fields.Many2one('stock.location', "Related Location", required=True)
+
+
+    def action_validate_revaluation(self):
+        self.reason = self.current_quantity_svl
+        super(StockValuationLayerRevaluation, self).action_validate_revaluation()
+        
