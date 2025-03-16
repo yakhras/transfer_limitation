558f064b3d15fdbaaebbbf38d15e86db4826ba47
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-16 15:31:13 +0300

    edt

--- user_tracking/models/user_session.py
+++ user_tracking/models/user_session.py
@@ -121,7 +121,7 @@ class BaseModelTracking(models.AbstractModel):
                 'session_id': session.id,
                 'rec_name': model_description,  # Store model's readable name
                 'model': self._name,  # Store technical model name
-                'res_id': record.id,
+                # 'res_id': record.id,
                 # 'date': record.create_date,  # Store actual creation date
             } for record in records]
 
