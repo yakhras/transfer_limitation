218749e05b3ebfacac9e425747c759768997e7d4
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-16 11:11:20 +0300

    edt

--- user_tracking/models/user_session.py
+++ user_tracking/models/user_session.py
@@ -13,12 +13,18 @@ class UserSession(models.Model):
     user_id = fields.Many2one('res.users', string="User", required=True, default=lambda self: self.env.user)
     login_date = fields.Datetime(related='user_id.login_date', string="Login Date")
 
-    def create_session(self):
-        """ Automatically create a session record for the logged-in user with the login_date from res.users """
-        user = self.env.user
+ 
+    
 
-        # Create session record, using login_date from res.users
-        return self.create({
-            'user_id': user.id,
-            'login_date': user.login_date or fields.Datetime.now(),  # Fallback if login_date is not available
-        })
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    
+    def write(self, values):
+        """ Create a new session record when the login_date is updated """
+        if 'login_date' in values:
+            # Call the session creation method when login_date is updated
+            self.env['user.session'].create(self.id, values['login_date'])
+        
+        # Ensure the normal write process happens
+        return super(ResUsers, self).write(values)
