b84e8827253bff2fa92f63ef2d421962f3a721db
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-18 10:44:04 +0300

    edt

--- user_tracking/models/restrict_create.py
+++ user_tracking/models/restrict_create.py
@@ -1,15 +1,37 @@
 from odoo import models, api
 
-class IrUIView(models.Model):
+# class IrUIView(models.Model):
+#     _inherit = "ir.ui.view"
+
+#     @api.model
+#     def _apply_ir_rules(self, *args, **kwargs):
+#         """Override method to disable 'Create' for specific users in tree views."""
+#         res = super()._apply_ir_rules(*args, **kwargs)
+        
+#         restricted_user_id = 17  # Replace with the user ID you want to restrict
+
+#         if self.env.user.id == restricted_user_id and self.type == "tree":
+#             self.write({'create': False})  # Disables the "Create" button in tree views
+#         return res
+
+
+
+
+class RestrictCreateView(models.Model):
     _inherit = "ir.ui.view"
 
     @api.model
-    def _apply_ir_rules(self, *args, **kwargs):
-        """Override method to disable 'Create' for specific users in tree views."""
-        res = super()._apply_ir_rules(*args, **kwargs)
-        
-        restricted_user_id = 17  # Replace with the user ID you want to restrict
+    def fields_view_get(self, view_id=None, view_type="form", toolbar=False, submenu=False):
+        """Override to disable 'Create' in tree views for a specific user."""
+        res = super().fields_view_get(view_id, view_type, toolbar, submenu)
 
-        if self.env.user.id == restricted_user_id and self.type == "tree":
-            self.write({'create': False})  # Disables the "Create" button in tree views
+        restricted_user_id = 17  # Change this to the user ID to restrict
+
+        if self.env.user.id == restricted_user_id and view_type == "tree":
+            # Modify the XML to set create="0"
+            doc = self.env["ir.qweb"]._render_xml(res["arch"])
+            for tree in doc.findall(".//tree"):
+                tree.set("create", "0")
+            res["arch"] = self.env["ir.qweb"]._to_string(doc)
+
         return res
