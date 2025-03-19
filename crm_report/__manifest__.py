a58a75f494d5c25fd249160e7b889c7f276cac82
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-19 15:10:15 +0300

    edt

---
+++ crm_report/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CRM Report",
+
+    'summary': """
+        CRM Report""",
+
+    'description': """
+        CRM Report. 
+    """,
+
+    'author': "Yaser Akhras",
+    #'website': "https://www.yaserakhras.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    
+    'version': '15.0.1.0',
+    'installable': True,
+    'application': True,
+    'license': 'AGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['crm']  ,
+
+    # always loaded
+    'data': [
+        "views/crm_report.xml",
+    ],
+}
---
+++ crm_report/models/crm_activity_report.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class ActivityReport(models.Model):
+
+    _inherit = "crm.activity.report"
+
+
+    lead_id = fields.Many2one('crm.lead', "Opportunity", readonly=False)
+
