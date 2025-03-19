499ccd927bdb157ff40987f5acb7727b6541808b
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-19 10:54:31 +0300

    edt

--- user_tracking/models/__init__.py
+++ user_tracking/models/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from . import user_session
-from . import res_users
-from . import base_model_tracking
-from . import user_session_line
-from . import restrict_create
+# from . import user_session
+# from . import res_users
+# from . import base_model_tracking
+# from . import user_session_line
+# from . import restrict_create
+
+from . import crm_activity_report
