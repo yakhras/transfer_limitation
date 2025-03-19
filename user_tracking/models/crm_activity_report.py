7b32af236e90874aacf6a6571a8b658eefd68e29
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-19 13:54:10 +0300

    edt

--- user_tracking/models/crm_activity_report.py
+++ user_tracking/models/crm_activity_report.py
@@ -10,13 +10,3 @@ class ActivityReport(models.Model):
 
     lead_id = fields.Many2one('crm.lead', "Opportunity", readonly=False)
 
-
-
-    # def open_related_lead(self):
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'res_model': 'crm.lead',
-    #         'view_mode': 'form',
-    #         'res_id': self.lead_id.id,
-    #         'target': 'current',
-    #     }
