af506e2b39a572b37bcf477cacaa988448fbb172
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-19 11:26:46 +0300

    edt

--- user_tracking/models/crm_activity_report.py
+++ user_tracking/models/crm_activity_report.py
@@ -2,11 +2,11 @@
 
 from odoo import models, fields
 
+
 class ActivityReport(models.Model):
-    """ CRM Lead Analysis """
 
     _inherit = "crm.activity.report"
 
 
-
     lead_id = fields.Many2one('crm.lead', "Opportunity", readonly=False)
+    
