944da4b164a1febc316acb4cc7b6a07ba54d8c8f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-20 10:27:58 +0300

    edt

--- crm_report/models/crm_activity_report.py
+++ crm_report/models/crm_activity_report.py
@@ -13,4 +13,27 @@ class ActivityReport(models.Model):
         string='field_nameLast Stage Update',
         related='lead_id.date_last_stage_update', index=True, store=True, readonly=True)
     
-
+    def _select(self):
+        return """
+            SELECT
+                m.id,
+                l.create_date AS lead_create_date,
+                l.date_last_stage_update AS last_stage_update,
+                l.date_conversion,
+                l.date_deadline,
+                l.date_closed,
+                m.subtype_id,
+                m.mail_activity_type_id,
+                m.author_id,
+                m.date,
+                m.body,
+                l.id as lead_id,
+                l.user_id,
+                l.team_id,
+                l.country_id,
+                l.company_id,
+                l.stage_id,
+                l.partner_id,
+                l.type as lead_type,
+                l.active
+        """
