a39946cde2d34e220189ad243b13c5d4b7f61357
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-22 16:04:27 +0300

    edt

--- multi_email_signature/models/user_email_signature.py
+++ multi_email_signature/models/user_email_signature.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import ast
+import base64
 
 from odoo import _, api, fields, models, tools, Command
 from odoo.exceptions import UserError
+from odoo.tools import email_re
 
 
 class UserEmailSignature(models.Model):
@@ -37,7 +39,7 @@ class MailComposeMessageInherited(models.TransientModel):
         # Therefore, we can set the description in the context from the beginning to avoid falling
         # back on the regular display_name retrieved in '_notify_prepare_template_context'.
         
-        result = self.env['res.users.email.signature'].search([('user_id', '=', self.env.user.id)], limit=1)
+        # result = self.env['res.users.email.signature'].search([('user_id', '=', self.env.user.id)], limit=1)
         model_description = self._context.get('model_description')
         for wizard in self:
             # Duplicate attachments linked to the email.template.
@@ -89,7 +91,7 @@ class MailComposeMessageInherited(models.TransientModel):
                 # to create lots of emails in sudo as it is consdiered as a technical model.
                 batch_mails_sudo = self.env['mail.mail'].sudo()
                 all_mail_values = wizard.get_mail_values(res_ids)
-                result.result = all_mail_values
+                # result.result = all_mail_values
                 for res_id, mail_values in all_mail_values.items():
                     if wizard.composition_mode == 'mass_mail':
                         batch_mails_sudo |= self.env['mail.mail'].sudo().create(mail_values)
@@ -114,3 +116,130 @@ class MailComposeMessageInherited(models.TransientModel):
 
                 if wizard.composition_mode == 'mass_mail':
                     batch_mails_sudo.send(auto_commit=auto_commit)
+
+
+    def get_mail_values(self, res_ids):
+        """Generate the values that will be used by send_mail to create mail_messages
+        or mail_mails. """
+        result = self.env['res.users.email.signature'].search([('user_id', '=', self.env.user.id)], limit=1)
+
+        self.ensure_one()
+        results = dict.fromkeys(res_ids, False)
+        rendered_values = {}
+        mass_mail_mode = self.composition_mode == 'mass_mail'
+
+        # render all template-based value at once
+        if mass_mail_mode and self.model:
+            rendered_values = self.render_message(res_ids)
+        # compute alias-based reply-to in batch
+        reply_to_value = dict.fromkeys(res_ids, None)
+        if mass_mail_mode and not self.reply_to_force_new:
+            records = self.env[self.model].browse(res_ids)
+            reply_to_value = records._notify_get_reply_to(default=False)
+            # when having no specific reply-to, fetch rendered email_from value
+            for res_id, reply_to in reply_to_value.items():
+                if not reply_to:
+                    reply_to_value[res_id] = rendered_values.get(res_id, {}).get('email_from', False)
+
+        for res_id in res_ids:
+            # static wizard (mail.message) values
+            mail_values = {
+                'subject': self.subject,
+                'body': self.body or '',
+                'parent_id': self.parent_id and self.parent_id.id,
+                'partner_ids': [partner.id for partner in self.partner_ids],
+                'attachment_ids': [attach.id for attach in self.attachment_ids],
+                'author_id': self.author_id.id,
+                'email_from': self.email_from,
+                'record_name': self.record_name,
+                'reply_to_force_new': self.reply_to_force_new,
+                'mail_server_id': self.mail_server_id.id,
+                'mail_activity_type_id': self.mail_activity_type_id.id,
+            }
+
+            # mass mailing: rendering override wizard static values
+            if mass_mail_mode and self.model:
+                record = self.env[self.model].browse(res_id)
+                mail_values['headers'] = record._notify_email_headers()
+                # keep a copy unless specifically requested, reset record name (avoid browsing records)
+                mail_values.update(is_notification=not self.auto_delete_message, model=self.model, res_id=res_id, record_name=False)
+                # auto deletion of mail_mail
+                if self.auto_delete or self.template_id.auto_delete:
+                    mail_values['auto_delete'] = True
+                # rendered values using template
+                email_dict = rendered_values[res_id]
+                mail_values['partner_ids'] += email_dict.pop('partner_ids', [])
+                mail_values.update(email_dict)
+                if not self.reply_to_force_new:
+                    mail_values.pop('reply_to')
+                    if reply_to_value.get(res_id):
+                        mail_values['reply_to'] = reply_to_value[res_id]
+                if self.reply_to_force_new and not mail_values.get('reply_to'):
+                    mail_values['reply_to'] = mail_values['email_from']
+                # mail_mail values: body -> body_html, partner_ids -> recipient_ids
+                mail_values['body_html'] = mail_values.get('body', '')
+                mail_values['recipient_ids'] = [Command.link(id) for id in mail_values.pop('partner_ids', [])]
+
+                # process attachments: should not be encoded before being processed by message_post / mail_mail create
+                mail_values['attachments'] = [(name, base64.b64decode(enc_cont)) for name, enc_cont in email_dict.pop('attachments', list())]
+                attachment_ids = []
+                for attach_id in mail_values.pop('attachment_ids'):
+                    new_attach_id = self.env['ir.attachment'].browse(attach_id).copy({'res_model': self._name, 'res_id': self.id})
+                    attachment_ids.append(new_attach_id.id)
+                attachment_ids.reverse()
+                mail_values['attachment_ids'] = self.env['mail.thread'].with_context(attached_to=record)._message_post_process_attachments(
+                    mail_values.pop('attachments', []),
+                    attachment_ids,
+                    {'model': 'mail.message', 'res_id': 0}
+                )['attachment_ids']
+
+            results[res_id] = mail_values
+        
+        result.result = results
+        results = self._process_state(results)
+        return results
+
+
+    def _process_state(self, mail_values_dict):
+        recipients_info = self._process_recipient_values(mail_values_dict)
+        blacklist_ids = self._get_blacklist_record_ids(mail_values_dict)
+        optout_emails = self._get_optout_emails(mail_values_dict)
+        done_emails = self._get_done_emails(mail_values_dict)
+        # in case of an invoice e.g.
+        mailing_document_based = self.env.context.get('mailing_document_based')
+
+        for record_id, mail_values in mail_values_dict.items():
+            recipients = recipients_info[record_id]
+            # when having more than 1 recipient: we cannot really decide when a single
+            # email is linked to several to -> skip that part. Mass mailing should
+            # anyway always have a single recipient per record as this is default behavior.
+            if len(recipients['mail_to']) > 1:
+                continue
+
+            mail_to = recipients['mail_to'][0] if recipients['mail_to'] else ''
+            mail_to_normalized = recipients['mail_to_normalized'][0] if recipients['mail_to_normalized'] else ''
+
+            # prevent sending to blocked addresses that were included by mistake
+            # blacklisted or optout or duplicate -> cancel
+            if record_id in blacklist_ids:
+                mail_values['state'] = 'cancel'
+                mail_values['failure_type'] = 'mail_bl'
+                # Do not post the mail into the recipient's chatter
+                mail_values['is_notification'] = False
+            elif optout_emails and mail_to in optout_emails:
+                mail_values['state'] = 'cancel'
+                mail_values['failure_type'] = 'mail_optout'
+            elif done_emails and mail_to in done_emails and not mailing_document_based:
+                mail_values['state'] = 'cancel'
+                mail_values['failure_type'] = 'mail_dup'
+            # void of falsy values -> error
+            elif not mail_to:
+                mail_values['state'] = 'cancel'
+                mail_values['failure_type'] = 'mail_email_missing'
+            elif not mail_to_normalized or not email_re.findall(mail_to):
+                mail_values['state'] = 'cancel'
+                mail_values['failure_type'] = 'mail_email_invalid'
+            elif done_emails is not None and not mailing_document_based:
+                done_emails.append(mail_to)
+
+        return mail_values_dict
