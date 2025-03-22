c46c46a94b20186adc5b348f480531c46fd98c58
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-22 14:10:41 +0300

    edt

--- multi_email_signature/models/user_email_signature.py
+++ multi_email_signature/models/user_email_signature.py
@@ -11,4 +11,10 @@ class UserEmailSignature(models.Model):
     email = fields.Char(string='Email Address', required=True)
     signature_name = fields.Char(string='Signature Name', required=True, help='A descriptive name for the signature')
     signature = fields.Html(string='Signature', required=True)
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    email_signatures = fields.One2many('res.users.email.signature', 'user_id', string='Email Signatures')
     
