21bd3067ef39c74ba40e000dd4a070e71b1a95c5
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-03-26 10:21:12 +0300

    edt

--- product_doc/static/src/project_task_kanban.js
+++ product_doc/static/src/project_task_kanban.js
@@ -1,66 +1,119 @@
 /** @odoo-module **/
 
+import KanbanColumn from 'web.KanbanColumn';
+import KanbanView from 'web.KanbanView';
+import viewRegistry from 'web.view_registry';
+import {ProjectTaskKanbanRenderer}from '@project/js/project_kanban';
+
 console.log('new module')
 
-import tour from 'web_tour.tour';
-
-tour.register('personal_stage_tour', {
-    test: true,
-    url: '/web',
-},
-[tour.stepUtils.showAppsMenuItem(), {
-    trigger: '.o_app[data-menu-xmlid="project.menu_main_pm"]',
-}, {
-    content: "Open Pig Project",
-    trigger: '.o_kanban_record:contains("Pig")',
-}, {
-    // Default is grouped by stage, user should not be able to create/edit a column
-    content: "Check that there is no create column",
-    trigger: ".o_column_quick_create",
-    run: function () {},
-}, {
-    content: "Check that there is no create column",
-    trigger: ".o_column_edit",
-    run: function () {},
-}, {
-    content: "Check that there is no create column",
-    trigger: ".o_column_delete",
-    run: function () {},
-}, {
-    content: "Go to my tasks", // My tasks is grouped by personal stage by default
-    trigger: 'a[data-menu-xmlid="project.menu_project_management"]',
-}, {
-    content: "Check that we can create a new stage",
-    trigger: '.o_column_quick_create .o_quick_create_folded'
-}, {
-    content: "Create a new personal stage",
-    trigger: 'input[placeholder="Column title"]',
-    run: 'text Never',
-}, {
-    content: "Confirm create",
-    trigger: '.o_kanban_add',
-}, {
-    content: "Check that column exists",
-    trigger: '.o_kanban_header:contains("Never")',
-    run: function () {},
-}, {
-    content: 'Open column edit dropdown',
-    trigger: '.o_kanban_header:eq(0)',
-    run: function () {
-        $('.o_kanban_config.dropdown .dropdown-toggle').eq(0).click();
+const ProjectTaskKanbanColumn = KanbanColumn.extend({
+    /**
+     * @override
+     * @private
+     */
+    _onDeleteColumn: function (event) {
+        if (this.groupedBy === 'stage_id') {
+            event.preventDefault();
+            this.trigger_up('kanban_column_delete_wizard');
+        } else {
+            this._super(...arguments);
+        }
     },
-}, {
-    content: "Try editing inbox",
-    trigger: ".dropdown-item.o_column_edit",
-}, {
-    content: "Change title",
-    trigger: 'input.o_field_char[name="name"]',
-    run: 'text  (Todo)',
-}, {
-    content: "Save changes",
-    trigger: '.btn-primary:contains("Save")',
-}, {
-    content: "Check that column was updated",
-    trigger: '.o_kanban_header:contains("Todo")',
-}]);
+
+    /**
+     * Open alternative view when editing personal stages.
+     *
+     * @private
+     * @override
+     */
+    _onEditColumn: function (event) {
+        if (this.groupedBy !== 'personal_stage_type_ids') {
+            this._super(...arguments);
+            return;
+        }
+        event.preventDefault();
+        const context = Object.assign({}, this.getSession().user_context, {
+            form_view_ref: 'project.personal_task_type_edit',
+        });
+        new view_dialogs.FormViewDialog(this, {
+            res_model: this.relation,
+            res_id: this.id,
+            context: context,
+            title: _t("Edit Personal Stage"),
+            on_saved: this.trigger_up.bind(this, 'reload'),
+        }).open();
+    },
+});
+
+const CustomProjectTaskKanbanRenderer = ProjectTaskKanbanRenderer.extend({
+    config: Object.assign({}, KanbanRenderer.prototype.config, {
+        KanbanColumn: ProjectTaskKanbanColumn,
+    }),
+
+    init: function () {
+        this._super.apply(this, arguments);
+        this.isProjectManager = false;
+    },
+
+    willStart: function () {
+        const superPromise = this._super.apply(this, arguments);
+
+        const isProjectManager = this.getSession().user_has_group('project.group_project_user').then((hasGroup) => {
+            this.isProjectManager = hasGroup;
+            this._setState();
+            return Promise.resolve();
+        });
+
+        return Promise.all([superPromise, isProjectManager]);
+    },
+
+    /**
+     * Allows record drag when grouping by `personal_stage_type_ids`
+     *
+     * @override
+     */
+    _setState() {
+        this._super(...arguments);
+        const groupedBy = this.state.groupedBy[0];
+        const groupByFieldName = viewUtils.getGroupByField(groupedBy);
+        const field = this.state.fields[groupByFieldName] || {};
+        const fieldInfo = this.state.fieldsInfo.kanban[groupByFieldName] || {};
+
+        const grouped_by_date = ["date", "datetime"].includes(field.type);
+        const grouped_by_m2m = field.type === "many2many";
+        const readonly = !!field.readonly || !!fieldInfo.readonly;
+        const groupedByPersonalStage = (groupByFieldName === 'personal_stage_type_ids');
+
+        const draggable = !readonly && (!grouped_by_m2m || groupedByPersonalStage) &&
+            (!grouped_by_date || fieldInfo.allowGroupRangeValue);
+
+        // When grouping by personal stage we allow any project user to create
+        let editable = this.columnOptions.editable;
+        let deletable = this.columnOptions.deletable;
+        if (['stage_id', 'personal_stage_type_ids'].includes(groupByFieldName)) {
+            this.groupedByM2O = groupedByPersonalStage || this.groupedByM2O;
+            const allow_crud = this.isProjectManager || groupedByPersonalStage;
+            this.createColumnEnabled = editable = deletable = allow_crud;
+        }
+
+        Object.assign(this.columnOptions, {
+            draggable,
+            grouped_by_m2o: this.groupedByM2O,
+            editable: editable,
+            deletable: deletable,
+        });
+    }
+});
+
+
+const CustomProjectKanbanView = KanbanView.extend({
+    config: _.extend({}, KanbanView.prototype.config, {
+        Renderer: CustomProjectTaskKanbanRenderer,
+    }),
+});
+
+
+viewRegistry.add('project_task_kanban', CustomProjectKanbanView);
+
  console.log('thanks')
