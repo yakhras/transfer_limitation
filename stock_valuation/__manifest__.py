0e05d525b4070f1311d4751e0a641478c345a250
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-09 17:54:52 +0300

    edt

--- stock_valuation/__manifest__.py
+++ stock_valuation/__manifest__.py
@@ -21,7 +21,7 @@
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['stock_landed_costs'],
+    'depends': ['stock_landed_costs','base'],
 
     # always loaded
     'data': [
