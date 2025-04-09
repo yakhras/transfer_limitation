cfc771dfa266f312be5d479e40a51123f1f2fc88
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-09 18:26:57 +0300

    edt

--- stock_valuation/models/res_users.py
+++ stock_valuation/models/res_users.py
@@ -8,19 +8,25 @@ class Users(models.Model):
     _inherit = 'res.users'
 
 
-    property_location_id = fields.Many2many(
+    default_location_id = fields.Many2one(
         'stock.location',
-        compute='_compute_warehouse_location_ids',
-        string='Warehouse Locations'
+        string='Default Location',
+        domain="[('id', 'in', available_location_ids)]"
+    )
+
+    available_location_ids = fields.Many2many(
+        'stock.location',
+        compute='_compute_available_location_ids',
+        string='Available Locations'
     )
 
     @api.depends('property_warehouse_id')
-    def _compute_warehouse_location_ids(self):
+    def _compute_available_location_ids(self):
         for user in self:
             if user.property_warehouse_id and user.property_warehouse_id.view_location_id:
-                user.property_location_id = self.env['stock.location'].search([
+                user.available_location_ids = self.env['stock.location'].search([
                     ('id', 'child_of', user.property_warehouse_id.view_location_id.id),
                     ('usage', '!=', 'view')
                 ])
             else:
-                user.property_location_id = False
+                user.available_location_ids = False
