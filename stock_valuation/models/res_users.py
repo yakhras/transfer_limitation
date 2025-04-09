76d57a46e7345fe19a8b4f62d859894202e8ce45
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-09 18:20:06 +0300

    edt

--- stock_valuation/models/res_users.py
+++ stock_valuation/models/res_users.py
@@ -1,10 +1,26 @@
-from odoo import models, fields
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
 
 
 
 class Users(models.Model):
-    _inherit = ['res.users']
+    _inherit = 'res.users'
 
 
-    property_location_id = fields.Many2one('stock.location', string='Default Location')
+    warehouse_location_ids = fields.Many2many(
+        'stock.location',
+        compute='_compute_warehouse_location_ids',
+        string='Warehouse Locations'
+    )
+
+    @api.depends('property_warehouse_id')
+    def _compute_warehouse_location_ids(self):
+        for user in self:
+            if user.property_warehouse_id and user.property_warehouse_id.view_location_id:
+                user.warehouse_location_ids = self.env['stock.location'].search([
+                    ('id', 'child_of', user.property_warehouse_id.view_location_id.id),
+                    ('usage', '!=', 'view')
+                ])
+            else:
+                user.warehouse_location_ids = False
