91ebe95dd30deebe3a2b61c8d1c3a971437aee15
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-10 10:41:11 +0300

    edt

--- stock_valuation/models/product_product.py
+++ stock_valuation/models/product_product.py
@@ -171,3 +171,23 @@ class ProductLocationCost(models.Model):
     location_id = fields.Many2one('stock.location', string='Location', required=True)
     cost = fields.Float('Cost', digits='Product Price')
 
+
+    def create_missing_cost_records(self):
+        Product = self.env['product.product']
+        StockQuant = self.env['stock.quant']
+        LocationCost = self.env['product.location.cost']
+
+        all_products = Product.search([])
+        for product in all_products:
+            quants = StockQuant.search([('product_id', '=', product.id)])
+            for quant in quants:
+                domain = [
+                    ('product_id', '=', product.id),
+                    ('location_id', '=', quant.location_id.id)
+                ]
+                if not LocationCost.search_count(domain):
+                    LocationCost.create({
+                        'product_id': product.id,
+                        'location_id': quant.location_id.id,
+                        'cost': product.standard_price,
+                    })
