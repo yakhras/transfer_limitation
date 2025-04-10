b466406146d1081f9b56d9d40bdfda759b5f0d2a
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-10 17:45:25 +0300

    edt

--- stock_valuation/models/stock_move.py
+++ stock_valuation/models/stock_move.py
@@ -113,3 +113,25 @@ class StockMove(models.Model):
         # # Store the result
         # self.result = f"Execution Time: {execution_time:.5f} seconds"
 
+    def _create_out_svl(self, forced_quantity=None):
+            """Create a `stock.valuation.layer` from `self`.
+
+            :param forced_quantity: under some circunstances, the quantity to value is different than
+                the initial demand of the move (Default value = None)
+            """
+            svl_vals_list = []
+            for move in self:
+                move = move.with_company(move.company_id)
+                valued_move_lines = move._get_out_move_lines()
+                valued_quantity = 0
+                for valued_move_line in valued_move_lines:
+                    valued_quantity += valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, move.product_id.uom_id)
+                if float_is_zero(forced_quantity or valued_quantity, precision_rounding=move.product_id.uom_id.rounding):
+                    continue
+                svl_vals = move.product_id.with_context(location_de_id = self.location_id.id)._prepare_out_svl_vals(forced_quantity or valued_quantity, move.company_id)
+                svl_vals.update(move._prepare_common_svl_vals())
+                if forced_quantity:
+                    svl_vals['description'] = 'Correction of %s (modification of past move)' % move.picking_id.name or move.name
+                svl_vals['description'] += svl_vals.pop('rounding_adjustment', '')
+                svl_vals_list.append(svl_vals)
+            return self.env['stock.valuation.layer'].sudo().create(svl_vals_list)
