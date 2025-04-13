a53b0f77fadf768f9cc4a03dce523918a7baa95f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-13 11:55:28 +0300

    edt

--- pos_invoice_t/static/src/js/Screens/SaleOrderScreen/SaleOrderScreen.js
+++ pos_invoice_t/static/src/js/Screens/SaleOrderScreen/SaleOrderScreen.js
@@ -35,18 +35,6 @@ odoo.define('pos_invoice_t.SaleOrderScreen', function (require) {
                                     item: false,
                                     icon: 'fa fa-close',
                                 },
-                                {
-                                    id:3, 
-                                    label: this.env._t("Formal Invoice"), 
-                                    item: true,
-                                    icon: 'fa fa-check-circle',
-                                }, 
-                                {
-                                    id:4, 
-                                    label: this.env._t("Informal Invoice"), 
-                                    item: false,
-                                    icon: 'fa fa-close',
-                                },
                             ],
                     });
                 if (confirmed){
