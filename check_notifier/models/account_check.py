81deb3aa1ef190a2e6fb56adee6e338815536dbb
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-18 10:46:05 +0300

    edt

--- check_notifier/models/account_check.py
+++ check_notifier/models/account_check.py
@@ -19,4 +19,4 @@ class AccountCheck(models.Model):
             ('payment_date', '=', today)
         ])
         for record in self:
-            record.result_domain = all_today_checks
+            record.result_domain = today
