dbcbcc747024b77a6008d695ddf7595057eaee2f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-18 10:38:29 +0300

    edt

--- check_notifier/__manifest__.py
+++ check_notifier/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Yaser Akhras',
     'depends': ['l10n_tr_account_check'],
     'data': [
-        "security/ir.model.access.csv",
+        # "security/ir.model.access.csv",
         'views/check_report_queue_views.xml',
     #     'data/check_notifier_cron.xml',
     ],
