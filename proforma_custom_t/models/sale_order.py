cca40ffb32e125d4cbc2b725ee2f95db024c5af6
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-04-24 10:24:13 +0300

    edt

--- proforma_custom_t/models/sale_order.py
+++ proforma_custom_t/models/sale_order.py
@@ -77,3 +77,10 @@ class ResUsers(models.Model):
     default_picking_type_ids = fields.Many2many(
         'stock.picking.type', 'stock_picking_type_users_rel',
         'user_id', 'picking_type_id', string='Default Warehouse Operations', store=False)
+    
+    stock_location_ids = fields.Many2many(
+        'stock.location',
+        'location_security_stock_location_users',
+        'user_id',
+        'location_id',
+        'Stock Locations', store=False,)
