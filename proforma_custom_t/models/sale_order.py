98795cc14edb25a423d972f44a56e224eea80aa4
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-05-01 21:53:54 +0300

    Update sale_order.py

--- proforma_custom_t/models/sale_order.py
+++ proforma_custom_t/models/sale_order.py
@@ -96,10 +96,10 @@ class CrmLead(models.Model):
   @api.model
   def write(self, vals):
     if 'stage_id' in vals:
-      old_stages = {lead.id: lead.stage_id for lead in self}
+      
       res = super().write(vals)
       for lead in self:
-        if lead.stage_id and lead.id in old_stages and lead.stage_id != old_stages[lead.id]:
+        
           message = "HI"
           self.env['bus.bus']._sendone(
             self.env.user.partner_id.id,
