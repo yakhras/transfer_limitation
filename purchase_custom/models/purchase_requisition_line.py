866e6177bdc82da07933d086715726293dcf8358
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-05-14 13:11:18 +0300

    edt

--- purchase_custom/models/purchase_requisition_line.py
+++ purchase_custom/models/purchase_requisition_line.py
@@ -30,11 +30,6 @@ class PurchaseRequisitionLine(models.Model):
             })
 
     def _prepare_compute_all_values(self):
-        # Hook method to returns the different argument values for the
-        # compute_all method, due to the fact that discounts mechanism
-        # is not implemented yet on the purchase orders.
-        # This method should disappear as soon as this feature is
-        # also introduced like in the sales module.
         self.ensure_one()
         return {
             'price_unit': self.price_unit,
