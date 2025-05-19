82b4c7290f9f70f974b4f48b357d3a3f590648f8
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-05-19 15:26:57 +0300

    edt

--- stock_valuation/models/product_product.py
+++ stock_valuation/models/product_product.py
@@ -171,7 +171,7 @@ class ProductProduct(models.Model):
                 domain.append(('create_date', '<=', to_date))
             id = self.env.context.get('location_dest_id')
             if id:
-                domain.extend(['|', ('stock_move_id.location_dest_id.id', '=', id), ('stock_move_id.location_id.id', '=', id)])
+                
 
             groups = self.env['stock.valuation.layer'].read_group(domain, ['value:sum', 'quantity:sum'], ['product_id'], orderby='id')
             
@@ -337,7 +337,7 @@ class StockLocation(models.Model):
     def action_custom_svl_summary(self):
         for location in self:
             result_lines = []
-            domain = [['|',("stock_move_id.location_id.id","=",location.id),("stock_move_id.location_dest_id.id","=",location.id)]],
+            domain = ['|',("stock_move_id.location_id.id","=",location.id),("stock_move_id.location_dest_id.id","=",location.id)]
             groups = self.env['stock.valuation.layer'].read_group(domain, ['value:sum', 'quantity:sum'], ['product_id'], orderby='id')
             products = self.browse()
             for group in groups:
