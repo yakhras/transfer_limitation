65b66c3c06ee511234eaf797e72598a65b8f5670
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-05-29 13:06:17 +0300

    edt

--- stock_valuation/models/stock_move.py
+++ stock_valuation/models/stock_move.py
@@ -299,6 +299,7 @@ class StockPicking(models.Model):
                     move_line.signed_qty_done = -move_line.qty_done
                     move_line.balance = source_quant.quantity if source_quant else 0.0
                     move_line.operation = f"Transfer Out → {from_name}"
+                    move_line.warehouse_id = move_line.location_id.warehouse_id
 
                     # Create the copied line
                     copied_line = move_line.copy()
@@ -313,6 +314,7 @@ class StockPicking(models.Model):
                     copied_line.signed_qty_done = move_line.qty_done
                     copied_line.balance = dest_quant.quantity if dest_quant else 0.0
                     copied_line.operation = f"Transfer In → {to_name}"
+                    copied_line.warehouse_id = copied_line.location_dest_id.warehouse_id
 
         return res
 
