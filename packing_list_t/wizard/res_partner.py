d0425c42d8fe6155d1f4a7fe06240a4a858fbe87
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-15 14:28:44 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -103,8 +103,8 @@ class ResPartnerSaleReport(models.TransientModel):
             worksheet.write(row_num, 2, line.product_id.display_name, border_format)
             worksheet.write(row_num, 3, line.product_uom_qty, border_format)
             worksheet.write(row_num, 4, line.product_packaging_id.name, border_format)
-            worksheet.write(row_num, 5, line.product_id.net_weight, border_format)
-            worksheet.write(row_num, 6, line.product_id.gross_weight, border_format)  # Example gross weight calculation
+            worksheet.write(row_num, 5, line.net_weight, border_format)
+            worksheet.write(row_num, 6, line.gross_weight, border_format)  # Example gross weight calculation
 
 
         
