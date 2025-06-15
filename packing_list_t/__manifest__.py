51899adbd52a8c61fed3565bfad50bd19d26aeee
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-15 11:24:32 +0300

    edt

--- packing_list_t/__manifest__.py
+++ packing_list_t/__manifest__.py
@@ -25,6 +25,7 @@
 
     # always loaded
     'data': [
+        "wizard/res_partner_view.xml",
         "views/report_packing_template.xml",
         "views/custom_packing_template.xml",
         "views/sale_order_view.xml",
