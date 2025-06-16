894877835587966c4b46708fde04d7ce61853ac6
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-16 17:00:38 +0300

    edt

--- partner_balance/models/account_move_line_inherit.py
+++ partner_balance/models/account_move_line_inherit.py
@@ -1,6 +1,6 @@
 # # -*- coding: utf-8 -*-
 
-from odoo import models, api, _
+from odoo import models, api, _, fields
 from odoo.exceptions import UserError
 import logging
 
@@ -8,6 +8,12 @@ _logger = logging.getLogger(__name__)
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
+
+
+    cumulated_balance_amount_currency = fields.Monetary(string='Cumulated Amount Balance', store=False,
+        currency_field='company_currency_id',
+        compute='_compute_cumulated_amount_currency',
+        help="Cumulated amount currency balance depending on the domain and the order chosen in the view.")
 
     @api.model
     def write(self, vals):
@@ -62,3 +68,28 @@ class AccountMoveLine(models.Model):
             partner_balance._compute_balance()  # Call the method to recalculate balance
 
         return res
+    
+
+    @api.depends_context('order_cumulated_balance', 'domain_cumulated_balance')
+    def _compute_cumulated_amount_currency(self):
+        if not self.env.context.get('order_cumulated_balance'):
+            # We do not come from search_read, so we are not in a list view, so it doesn't make any sense to compute the cumulated balance
+            self.cumulated_balance = 0
+            return
+
+        # get the where clause
+        query = self._where_calc(list(self.env.context.get('domain_cumulated_balance') or []))
+        order_string = ", ".join(self._generate_order_by_inner(self._table, self.env.context.get('order_cumulated_balance'), query, reverse_direction=True))
+        from_clause, where_clause, where_clause_params = query.get_sql()
+        sql = """
+            SELECT account_move_line.id, SUM(account_move_line.amount_currency) OVER (
+                ORDER BY %(order_by)s
+                ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+            )
+            FROM %(from)s
+            WHERE %(where)s
+        """ % {'from': from_clause, 'where': where_clause or 'TRUE', 'order_by': order_string}
+        self.env.cr.execute(sql, where_clause_params)
+        result = {r[0]: r[1] for r in self.env.cr.fetchall()}
+        for record in self:
+            record.cumulated_balance = result[record.id]
