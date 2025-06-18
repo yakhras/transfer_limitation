df8adf2a594a93361594a16eb5739eeda4fec6f3
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-18 15:45:23 +0300

    edt

--- partner_balance/static/src/js/export_pdf_button.js
+++ partner_balance/static/src/js/export_pdf_button.js
@@ -18,6 +18,8 @@ var ExportPdfButtonListController = ListController.extend({
         const context = this.model.get('context');
         const order = this.model.get('order');
         const viewId = this.viewId;
+        console.log('domain', domain);
+        console.log('context', context);
         this._rpc({
             model: 'report.partner_balance.xlsx_report',
             method: 'button_export_xlsx',
