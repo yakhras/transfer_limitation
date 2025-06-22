109634e95dacd854b20162a1c759f8a3adec6eb7
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-22 12:06:12 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -119,7 +119,7 @@ class ResPartnerSaleReport(models.TransientModel):
         )
 
         # Seller and Buyer Information
-        row = 11 
+        row = 10 
         col_buyer = 1  
         worksheet.write(row, col_buyer, "Buyer:", font10_format)
         row += 1
@@ -134,9 +134,12 @@ class ResPartnerSaleReport(models.TransientModel):
             sale_order.partner_shipping_id.zip,
             sale_order.partner_shipping_id.country_id.name if sale_order.partner_shipping_id.country_id else None,
         ])
-        worksheet.write(row, col_buyer, ", ".join(buyer_address_parts), wrap_format)
+        worksheet.write(row, col_buyer, f"Address: {", ".join(buyer_address_parts)}", font10_format)
+        # worksheet.write(row, col_buyer, ", ".join(buyer_address_parts), wrap_format)
         row += 1
         worksheet.write(row, col_buyer, f"Phone: {sale_order.partner_id.phone or ''}", font10_format)
+
+        
 
         # Order Information
         date = sale_order.date_order.strftime('%Y-%m-%d') if sale_order.date_order else ""
