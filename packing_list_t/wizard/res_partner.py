22e70103364bb28277dc4efdc6baf5b491449df2
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-22 16:05:48 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -155,13 +155,13 @@ class ResPartnerSaleReport(models.TransientModel):
         worksheet.write(row, col_buyer, "Address:", bold_format)
         # worksheet.write(row, col_buyer+1, ", ".join(buyer_address_parts), wrap_format)
         buyer_address = ", ".join(buyer_address_parts)
-        worksheet.merge_range(11, 3, 11, 4, buyer_address, wrap_format)
+        worksheet.merge_range(11, 3, 11, 4, 11, 5, buyer_address, wrap_format)
 
         row += 1
         if sale_order.company_id.phone:
             worksheet.write(row, col_seller, "Phone:", bold_format)
             worksheet.write(row, col_seller + 1, sale_order.company_id.phone or '', font10_format)
-            
+
         if sale_order.partner_id.phone:
             worksheet.write(row, col_buyer, "Phone:", bold_format)
             worksheet.write(row, col_buyer+1, sale_order.partner_id.phone or '', font10_format)
