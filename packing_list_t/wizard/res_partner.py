ef93ea2731c540e6648c588181d9ece20ad42506
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-22 13:16:40 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -171,7 +171,7 @@ class ResPartnerSaleReport(models.TransientModel):
             ]
             for col, val in enumerate(data, start=0):
                 worksheet.write(idx, col, val, border_format)
-                # col_widths[col - 1] = max(col_widths[col - 1], len(val))
+                col_widths[col - 1] = max(col_widths[col - 1], len(val))
 
         # Set column widths with padding
         for i, width in enumerate(col_widths, start=1):
