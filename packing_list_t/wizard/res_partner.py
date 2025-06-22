f48ab834dbcc4bdc0749240d35919328ec179f87
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-22 10:24:23 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -71,7 +71,7 @@ class ResPartnerSaleReport(models.TransientModel):
         fp = BytesIO()
         file_name = "Packing List.xlsx"
 
-        # Create workbook and worksheet
+        # Create an Excel workbook and worksheet
         workbook = xlsxwriter.Workbook(fp, {"in_memory": True})
         worksheet = workbook.add_worksheet(sale_order.name)
         worksheet.set_paper(9)
@@ -79,14 +79,75 @@ class ResPartnerSaleReport(models.TransientModel):
         worksheet.fit_to_pages(1, 0)
 
         # Styles
-        font10 = {'font_size': 10}
+        font10 = {'font_size': 14}
         font10_format = workbook.add_format(font10)
         border_format = workbook.add_format({'border': 1, **font10})
         header_format = workbook.add_format({'border': 1, 'bold': True, **font10})
 
-        # Header & footer (same as before)...
+        # Header and Footer
+        logo_path = sale_order.company_id.logo
+        logo_data = base64.b64decode(logo_path)
+        tmp_logo_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
+        tmp_logo_file.write(logo_data)
+        tmp_logo_file.close()
+        worksheet.set_header(
+            '&L&B&18%s&R&G' % (sale_order.company_id.name or ''),
+            {
+                'image_right': tmp_logo_file.name,
+            }
+        )
 
-        # Seller/Buyer Info & Order Info (same as before)...
+        footer_address = sale_order.company_id.street2 or ''
+        if sale_order.company_id.street:
+            footer_address += ', ' + sale_order.company_id.street
+        if sale_order.company_id.city:
+            footer_address += ', ' + sale_order.company_id.city
+        if sale_order.company_id.state_id:
+            footer_address += ', ' + sale_order.company_id.state_id.name
+        if sale_order.company_id.country_id:
+            footer_address += ', ' + sale_order.company_id.country_id.name
+        worksheet.set_footer(
+            '&LPage &P'
+            '&C%s'
+            '&R%s' % (footer_address, sale_order.company_id.vat or '')
+        )
+
+        # Seller and Buyer Information
+        row = 13  
+        col_seller = 1  
+        col_buyer = 5   
+        worksheet.write(row, col_seller, "Seller:", font10_format)
+        worksheet.write(row, col_buyer, "Buyer:", font10_format)
+        row += 1
+        worksheet.write(row, col_seller, sale_order.company_id.name or "", font10_format)
+        worksheet.write(row, col_buyer, sale_order.partner_id.name or "", font10_format)
+        row += 1
+        seller_address_parts = filter(None, [
+            sale_order.company_id.street,
+            sale_order.company_id.street2,
+            sale_order.company_id.city,
+            sale_order.company_id.state_id.name if sale_order.company_id.state_id else None,
+            sale_order.company_id.zip,
+            sale_order.company_id.country_id.name if sale_order.company_id.country_id else None,
+        ])
+        buyer_address_parts = filter(None, [
+            sale_order.partner_shipping_id.street,
+            sale_order.partner_shipping_id.street2,
+            sale_order.partner_shipping_id.city,
+            sale_order.partner_shipping_id.state_id.name if sale_order.partner_shipping_id.state_id else None,
+            sale_order.partner_shipping_id.zip,
+            sale_order.partner_shipping_id.country_id.name if sale_order.partner_shipping_id.country_id else None,
+        ])
+        worksheet.write(row, col_seller, ", ".join(seller_address_parts), font10_format)
+        worksheet.write(row, col_buyer, ", ".join(buyer_address_parts), font10_format)
+        row += 1
+        worksheet.write(row, col_seller, f"Phone: {sale_order.company_id.phone or ''}", font10_format)
+        worksheet.write(row, col_buyer, f"Phone: {sale_order.partner_id.phone or ''}", font10_format)
+
+        # Order Information
+        date = sale_order.date_order.strftime('%Y-%m-%d') if sale_order.date_order else ""
+        worksheet.write('B11', f"Date: {date}", font10_format)
+        worksheet.write('F11', f"Order No: {sale_order.name}", font10_format)
 
         # Order Lines Table
         order_lines = sale_order.order_line
@@ -112,19 +173,21 @@ class ResPartnerSaleReport(models.TransientModel):
         # Set column widths with padding
         for i, width in enumerate(col_widths, start=1):
             worksheet.set_column(i, i, width + 2)
-
+            
         workbook.close()
 
-        attachment_id = self.env["ir.attachment"].create({
-            "name": file_name,
-            "type": "binary",
-            "datas": base64.encodebytes(fp.getvalue()),
-            "res_model": self._name,
-            "res_id": self.id,
-        })
-
+        attachment_id = self.env["ir.attachment"].create(
+            {
+                "name": file_name,
+                "type": "binary",
+                "datas": base64.encodebytes(fp.getvalue()),
+                "res_model": self._name,
+                "res_id": self.id,
+            }
+        )
         return {
             "type": "ir.actions.act_url",
-            "url": "/web/content/%s/%s/datas/%s" % ("ir.attachment", attachment_id.id, file_name),
+            "url": "/web/content/%s/%s/datas/%s"
+                % ("ir.attachment", attachment_id.id, file_name),
             "target": "self",
         }
