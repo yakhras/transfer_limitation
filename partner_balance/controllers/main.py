99af02d3727692850f3d195c4f330298c55edffe
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-23 18:09:08 +0300

    edt

--- partner_balance/controllers/main.py
+++ partner_balance/controllers/main.py
@@ -47,7 +47,7 @@ class ExcelExport(BaseExcelExport):
 
             export_data = records.export_data(field_names).get('datas',[])
             # response_data = self.from_data(columns_headers, export_data)
-            response_data = self.from_data(columns_headers, export_data, params.get('context', {}))
+            response_data = self.from_data(columns_headers, export_data, params.get('context', {}).get('partner_name', ''))
 
         # TODO: call `clean_filename` directly in `content_disposition`?
         return request.make_response(response_data,
