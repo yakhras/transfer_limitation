614b834c9186a61bb648768925bf70f9714db622
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-25 15:58:29 +0300

    edt

--- partner_balance/controllers/main.py
+++ partner_balance/controllers/main.py
@@ -108,23 +108,23 @@ class ExcelExport(BaseExcelExport):
     
 
 class ExportXlsxWriter(BaseExportXlsxWriter):
-    def __init__(self, field_names, row_count=0):
-        self.field_names = field_names
-        self.output = io.BytesIO()
-        self.workbook = xlsxwriter.Workbook(self.output, {'in_memory': True})
-        self.base_style = self.workbook.add_format({'text_wrap': True})
-        self.header_style = self.workbook.add_format({'bold': True, 'bg_color': "#f5f50e", 'border': 1})
-        self.header_bold_style = self.workbook.add_format({'text_wrap': True, 'bold': True, 'bg_color': "#f5f50e"})
-        self.date_style = self.workbook.add_format({'text_wrap': True, 'num_format': 'yyyy-mm-dd'})
-        self.datetime_style = self.workbook.add_format({'text_wrap': True, 'num_format': 'yyyy-mm-dd hh:mm:ss'})
-        self.worksheet = self.workbook.add_worksheet()
-        self.value = False
-        self.float_format = '#,##0.00'
-        decimal_places = [res['decimal_places'] for res in request.env['res.currency'].search_read([], ['decimal_places'])]
-        self.monetary_format = f'#,##0.{max(decimal_places or [2]) * "0"}'
+    # def __init__(self, field_names, row_count=0):
+    #     self.field_names = field_names
+    #     self.output = io.BytesIO()
+    #     self.workbook = xlsxwriter.Workbook(self.output, {'in_memory': True})
+    #     self.base_style = self.workbook.add_format({'text_wrap': True})
+    #     self.header_style = self.workbook.add_format({'bold': True, 'bg_color': "#f5f50e", 'border': 1})
+    #     self.header_bold_style = self.workbook.add_format({'text_wrap': True, 'bold': True, 'bg_color': "#f5f50e"})
+    #     self.date_style = self.workbook.add_format({'text_wrap': True, 'num_format': 'yyyy-mm-dd'})
+    #     self.datetime_style = self.workbook.add_format({'text_wrap': True, 'num_format': 'yyyy-mm-dd hh:mm:ss'})
+    #     self.worksheet = self.workbook.add_worksheet()
+    #     self.value = False
+    #     self.float_format = '#,##0.00'
+    #     decimal_places = [res['decimal_places'] for res in request.env['res.currency'].search_read([], ['decimal_places'])]
+    #     self.monetary_format = f'#,##0.{max(decimal_places or [2]) * "0"}'
 
-        if row_count > self.worksheet.xls_rowmax:
-            raise UserError(_('There are too many rows (%s rows, limit: %s) to export as Excel 2007-2013 (.xlsx) format. Consider splitting the export.') % (row_count, self.worksheet.xls_rowmax))
+    #     if row_count > self.worksheet.xls_rowmax:
+    #         raise UserError(_('There are too many rows (%s rows, limit: %s) to export as Excel 2007-2013 (.xlsx) format. Consider splitting the export.') % (row_count, self.worksheet.xls_rowmax))
 
     
     def write_header(self):
