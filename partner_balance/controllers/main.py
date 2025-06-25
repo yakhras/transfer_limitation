e25744c1d7eb350c52b6112299994c1b844a5bab
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-25 15:45:23 +0300

    edt

--- partner_balance/controllers/main.py
+++ partner_balance/controllers/main.py
@@ -128,7 +128,6 @@ class ExportXlsxWriter(BaseExportXlsxWriter):
 
     
     def write_header(self):
-        self.header_style = xlsxwriter.workbook(io.BytesIO(),{}).add_format({'bold': True, 'bg_color': "#eeefe9"})
         for i, fieldname in enumerate(self.field_names):
             self.write(4, i, fieldname, self.header_style)
         self.worksheet.set_column(0, i, 30) # around 220 pixels
