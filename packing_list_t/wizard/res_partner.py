d3f3b4768f52f6b43f70e04c3538098ab717a72a
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-27 18:48:13 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -88,7 +88,7 @@ class ResPartnerSaleReport(models.TransientModel):
         wrap_format = workbook.add_format({**font10, 'text_wrap': True, 'align': 'left', 'valign': 'top'})
         title_format = workbook.add_format({'font_size': 16, 'align': 'center', 'valign': 'center', 'bold': True, 'border': 1})
         label_format = workbook.add_format({'align': 'left', 'valign': 'center', 'font_size': 10, 'bold': True})
-        date_format = workbook.add_format({'align': 'left', 'valign': 'center','font_size': 10 })
+        date_format = workbook.add_format({'valign': 'left', 'align': 'center','font_size': 10 })
         name_format = workbook.add_format({'align': 'left', 'valign': 'center','font_size': 11 })
         worksheet.set_column('A:A', 8.43)
         worksheet.set_column('B:B', 8.43)
