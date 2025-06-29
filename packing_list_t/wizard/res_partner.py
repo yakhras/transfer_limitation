84c29755932cf7179773ff3d0fef7f770cc31802
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-29 12:28:48 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -115,7 +115,7 @@ class ResPartnerSaleReport(models.TransientModel):
         logo_path = sale_order.company_id.logo
         logo_data = base64.b64decode(logo_path)
         image = Image.open(io.BytesIO(logo_data))
-        resized_image = image.resize((120, 40))
+        resized_image = image.resize((222, 104))
         tmp_logo_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
         resized_image.save(tmp_logo_file, format='PNG')
         tmp_logo_file.close()
@@ -150,9 +150,7 @@ class ResPartnerSaleReport(models.TransientModel):
 
         worksheet.insert_image('G1:I6', tmp_logo_file.name, {
             'x_scale': 0.5,
-            'y_scale': 0.5,
-            'x_offset': 10,
-            'y_offset': 10,
+            'y_scale': 0.5
         })
 
         # # Title
