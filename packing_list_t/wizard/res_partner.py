6da419778091fe2584d41a982e754244d477cf46
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-29 14:25:47 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -18,7 +18,7 @@ class ResPartnerSaleReport(models.TransientModel):
 
     def action_generate_pdf_report(self):
         # self.env['res.partner'].action_view_move_line_report()
-        self.env['ir.actions.report']._get_report_from_name('packing_list_t.action_report_packing_sale_order')
+        return self.env['ir.actions.report']._get_report_from_name('packing_list_t.action_report_packing_sale_order').report_action(self)
         # partner_id = self.env.context.get("active_ids")
         # order_ids = self.env["sale.order"].search(
         #     [
