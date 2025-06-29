dcf39e9acbf6524faa35608f110d20424bc63258
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-06-29 12:57:41 +0300

    edt

--- packing_list_t/wizard/res_partner.py
+++ packing_list_t/wizard/res_partner.py
@@ -234,7 +234,7 @@ class ResPartnerSaleReport(models.TransientModel):
         worksheet.write(row, 0, "Note:", label_format)
         row += 1
         worksheet.set_row(row, 90)
-        worksheet.write(row, 0, sale_order.note or "", product_format)
+        worksheet.merge_range(row, 0, row, 8, sale_order.note or "", product_format)
             
 
         workbook.close()
