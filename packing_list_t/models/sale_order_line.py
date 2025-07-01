acdd03ee85a63118feb048a41859566d6f4cd111
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-01 12:30:49 +0300

    edt

--- packing_list_t/models/sale_order_line.py
+++ packing_list_t/models/sale_order_line.py
@@ -5,7 +5,7 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'   # Inherit the model
 
 
-    gross_weight = fields.Float('Cross Weight', 
+    gross_weight = fields.Float('Gross Weight', 
                                 digits='Stock Weight', 
                                 related='product_id.gross_weight',
                                 store=True,
