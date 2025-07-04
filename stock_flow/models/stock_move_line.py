51c4da0365d185e90b21b6bff00c6789e53b649e
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-04 11:22:17 +0300

    edt

--- stock_flow/models/stock_move_line.py
+++ stock_flow/models/stock_move_line.py
@@ -12,8 +12,8 @@ class StockMoveLine(models.Model):
 
     balance = fields.Float(string="Balance", store=True, readonly=True)
     
-    signed_qty_done = fields.Float(string="Signed Quantity Done", compute="_compute_signed_qty_done", store=True)
-    operation = fields.Char(string="Operation", compute="_compute_operation", store=True)
+    signed_qty_done = fields.Float(string="Signed Quantity Done",  store=True)
+    operation = fields.Char(string="Operation",  store=True)
     warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse', store=True)
 
 
@@ -33,56 +33,56 @@ class StockMoveLine(models.Model):
    
 
 
-    @api.depends('qty_done', 'location_id.usage', 'location_dest_id.usage')
-    def _compute_signed_qty_done(self):
-        for line in self:
-            if line.location_id.usage == 'internal' and line.location_dest_id.usage != 'internal':
-                # Outgoing
-                line.signed_qty_done = -line.qty_done
-                quant = self.env['stock.quant'].search([
-                    ('product_id', '=', line.product_id.id),
-                    ('location_id', '=', line.location_id.id),
-                ], limit=1) 
-                line.balance = quant.quantity if quant else 0.0
-            elif line.location_id.usage != 'internal' and line.location_dest_id.usage == 'internal':
-                # Incoming
-                line.signed_qty_done = line.qty_done
-                quant = self.env['stock.quant'].search([
-                    ('product_id', '=', line.product_id.id),
-                    ('location_id', '=', line.location_dest_id.id),
-                ], limit=1) 
-                line.balance = quant.quantity if quant else 0.0
+    # @api.depends('qty_done', 'location_id.usage', 'location_dest_id.usage')
+    # def _compute_signed_qty_done(self):
+    #     for line in self:
+    #         if line.location_id.usage == 'internal' and line.location_dest_id.usage != 'internal':
+    #             # Outgoing
+    #             line.signed_qty_done = -line.qty_done
+    #             quant = self.env['stock.quant'].search([
+    #                 ('product_id', '=', line.product_id.id),
+    #                 ('location_id', '=', line.location_id.id),
+    #             ], limit=1) 
+    #             line.balance = quant.quantity if quant else 0.0
+    #         elif line.location_id.usage != 'internal' and line.location_dest_id.usage == 'internal':
+    #             # Incoming
+    #             line.signed_qty_done = line.qty_done
+    #             quant = self.env['stock.quant'].search([
+    #                 ('product_id', '=', line.product_id.id),
+    #                 ('location_id', '=', line.location_dest_id.id),
+    #             ], limit=1) 
+    #             line.balance = quant.quantity if quant else 0.0
 
-    @api.depends('location_id', 'location_dest_id')
-    def _compute_operation(self):
-        for line in self:
-            # Skip if already set during duplication (e.g., "Transfer In"/"Transfer Out")
-            if line.operation in ('Transfer In', 'Transfer Out'):
-                continue
+    # @api.depends('location_id', 'location_dest_id')
+    # def _compute_operation(self):
+    #     for line in self:
+    #         # Skip if already set during duplication (e.g., "Transfer In"/"Transfer Out")
+    #         if line.operation in ('Transfer In', 'Transfer Out'):
+    #             continue
 
-            from_usage = line.location_id.usage
-            to_usage = line.location_dest_id.usage
-            from_name = line.location_id.display_name or ''
-            to_name = line.location_dest_id.display_name or ''
+    #         from_usage = line.location_id.usage
+    #         to_usage = line.location_dest_id.usage
+    #         from_name = line.location_id.display_name or ''
+    #         to_name = line.location_dest_id.display_name or ''
 
-            if from_usage == 'supplier' and to_usage == 'internal':
-                line.operation = f"Buy → {to_name}"
-                line.warehouse_id = line.location_dest_id.warehouse_id
-            elif from_usage == 'internal' and to_usage == 'supplier':
-                line.operation = f"Return Buy → {from_name}"
-                line.warehouse_id = line.location_id.warehouse_id
-            elif from_usage == 'internal' and to_usage == 'customer':
-                line.operation = f"Sell → {from_name}"
-                line.warehouse_id = line.location_id.warehouse_id
-            elif from_usage == 'customer' and to_usage == 'internal':
-                line.operation = f"Return Sell → {to_name}"
-                line.warehouse_id = line.location_dest_id.warehouse_id
-            elif from_usage == 'internal' and to_usage == 'inventory':
-                line.operation = f"Scrap → {from_name}"
-                line.warehouse_id = line.location_id.warehouse_id
-            elif from_usage == 'inventory' and to_usage == 'internal':
-                line.operation = f"Adjastment → {to_name}"
-                line.warehouse_id = line.location_dest_id.warehouse_id
+    #         if from_usage == 'supplier' and to_usage == 'internal':
+    #             line.operation = f"Buy → {to_name}"
+    #             line.warehouse_id = line.location_dest_id.warehouse_id
+    #         elif from_usage == 'internal' and to_usage == 'supplier':
+    #             line.operation = f"Return Buy → {from_name}"
+    #             line.warehouse_id = line.location_id.warehouse_id
+    #         elif from_usage == 'internal' and to_usage == 'customer':
+    #             line.operation = f"Sell → {from_name}"
+    #             line.warehouse_id = line.location_id.warehouse_id
+    #         elif from_usage == 'customer' and to_usage == 'internal':
+    #             line.operation = f"Return Sell → {to_name}"
+    #             line.warehouse_id = line.location_dest_id.warehouse_id
+    #         elif from_usage == 'internal' and to_usage == 'inventory':
+    #             line.operation = f"Scrap → {from_name}"
+    #             line.warehouse_id = line.location_id.warehouse_id
+    #         elif from_usage == 'inventory' and to_usage == 'internal':
+    #             line.operation = f"Adjastment → {to_name}"
+    #             line.warehouse_id = line.location_dest_id.warehouse_id
    
 
 
