cc299bb332905426373458cfe07369b5e3722098
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-06 15:44:59 +0300

    edt

--- billed_product/models/report_vendor_invoice.py
+++ billed_product/models/report_vendor_invoice.py
@@ -16,6 +16,8 @@ class ReportVendorInvoice(models.Model):
     purchase_order_ref = fields.Char(string='PO Reference')
     currency_id = fields.Many2one('res.currency', string='Currency')
     currency_rate = fields.Float(string='Currency Rate', digits=(12, 6))
+    warehouse_name = fields.Char(string='Warehouse')
+
 
 
     def init(self):
@@ -29,21 +31,28 @@ class ReportVendorInvoice(models.Model):
                     aml.id AS id,
                     am.name AS invoice_number,
                     am.invoice_date AS invoice_date,
-                    pt.name AS product_name,
+                    aml.product_id AS product_id,
                     aml.quantity AS quantity,
                     aml.price_unit::numeric(16, 6) AS price_unit,
                     aml.price_subtotal::numeric(16, 6) AS subtotal,
                     rp.name AS vendor_name,
                     am.invoice_origin AS purchase_order_ref,
                     am.currency_id AS currency_id,
-                    am.currency_rate AS currency_rate
+                    am.currency_rate AS currency_rate,
+                    sw.name AS warehouse_name
+
                 FROM account_move_line aml
                 JOIN account_move am ON aml.move_id = am.id
                 JOIN res_partner rp ON am.partner_id = rp.id
-                JOIN product_product pp ON aml.product_id = pp.id
-                JOIN product_template pt ON pp.product_tmpl_id = pt.id
+
+                LEFT JOIN purchase_order po ON po.name = am.invoice_origin
+                LEFT JOIN stock_picking sp ON sp.purchase_id = po.id
+                LEFT JOIN stock_picking_type spt ON sp.picking_type_id = spt.id
+                LEFT JOIN stock_warehouse sw ON spt.warehouse_id = sw.id
+
                 WHERE am.move_type = 'in_invoice'
                 AND aml.display_type IS NULL
             );
         """)
+
 
