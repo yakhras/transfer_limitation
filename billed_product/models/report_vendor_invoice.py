ade0ac29a7a60acec728cb1f5bf52b50da1744a8
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-06 14:53:16 +0300

    edt

--- billed_product/models/report_vendor_invoice.py
+++ billed_product/models/report_vendor_invoice.py
@@ -1,17 +1,20 @@
 from odoo import models, fields
 
+
 class ReportVendorInvoice(models.Model):
     _name = 'report.vendor.invoice'
     _description = 'Vendor Invoice Report'
     _auto = False
 
     invoice_number = fields.Char(string='Invoice Number')
+    invoice_date = fields.Date(string='Invoice Date')
     product_name = fields.Char(string='Product')
     quantity = fields.Float(string='Quantity')
-    price_unit = fields.Float(string='Price Unit')
-    subtotal = fields.Float(string='Subtotal')
+    price_unit = fields.Monetary(string='Price Unit', currency_field='currency_id')
+    subtotal = fields.Monetary(string='Subtotal', currency_field='currency_id')
     vendor_name = fields.Char(string='Vendor')
     purchase_order_ref = fields.Char(string='PO Reference')
+    currency_id = fields.Many2one('res.currency', string='Currency')
 
     def init(self):
         self.env.cr.execute("""
@@ -19,17 +22,20 @@ class ReportVendorInvoice(models.Model):
                 SELECT
                     aml.id AS id,
                     am.name AS invoice_number,
+                    am.invoice_date AS invoice_date,
                     pt.name AS product_name,
                     aml.quantity AS quantity,
                     aml.price_unit AS price_unit,
                     aml.price_subtotal AS subtotal,
                     rp.name AS vendor_name,
-                    am.invoice_origin AS purchase_order_ref
+                    am.invoice_origin AS purchase_order_ref,
+                    am.currency_id AS currency_id
                 FROM account_move_line aml
                 JOIN account_move am ON aml.move_id = am.id
                 JOIN res_partner rp ON am.partner_id = rp.id
                 JOIN product_product pp ON aml.product_id = pp.id
                 JOIN product_template pt ON pp.product_tmpl_id = pt.id
-                WHERE am.move_type = 'in_invoice' AND aml.display_type IS NULL
-            )
+                WHERE am.move_type = 'in_invoice'
+                AND aml.display_type IS NULL
+            );
         """)
