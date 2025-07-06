fc39568a2616056a08969cbdbe7f638f2c5ed6ad
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-06 09:28:12 +0300

    edt

--- stock_flow/models/stock_move_line.py
+++ stock_flow/models/stock_move_line.py
@@ -86,10 +86,13 @@ class StockMoveLine(models.Model):
    
 
 
+from odoo import fields, models, api
+
 class StockMoveLineReport(models.Model):
     _name = 'stock.move.line.report'
     _description = 'Stock Move Line Report'
-    _auto = False  # because we use a database view
+    _auto = False
+    _rec_name = 'move_line_id'
 
     move_line_id = fields.Many2one('stock.move.line', string="Original Move Line")
     product_id = fields.Many2one('product.product', string="Product")
@@ -101,5 +104,43 @@ class StockMoveLineReport(models.Model):
     signed_qty_done = fields.Float(string="Signed Qty")
     operation = fields.Char(string="Operation")
     direction = fields.Selection([('in', 'In'), ('out', 'Out')], string="Direction")
+
+    @api.model_cr
+    def init(self):
+        self.env.cr.execute("""DROP VIEW IF EXISTS stock_move_line_report""")
+        self.env.cr.execute("""
+            CREATE VIEW stock_move_line_report AS (
+                SELECT
+                    sml.id AS id,
+                    sml.id AS move_line_id,
+                    sml.product_id,
+                    sml.date,
+                    sml.location_id,
+                    sml.location_dest_id,
+                    sw.id AS warehouse_id,
+                    sml.qty_done,
+                    CASE
+                        WHEN sl.usage = 'internal' AND sld.usage != 'internal' THEN -sml.qty_done
+                        WHEN sl.usage != 'internal' AND sld.usage = 'internal' THEN sml.qty_done
+                        ELSE 0
+                    END AS signed_qty_done,
+                    sm.name AS operation,
+                    CASE
+                        WHEN sl.usage = 'internal' AND sld.usage != 'internal' THEN 'out'
+                        WHEN sl.usage != 'internal' AND sld.usage = 'internal' THEN 'in'
+                        ELSE NULL
+                    END AS direction
+                FROM stock_move_line sml
+                JOIN stock_move sm ON sm.id = sml.move_id
+                LEFT JOIN stock_location sl ON sml.location_id = sl.id
+                LEFT JOIN stock_location sld ON sml.location_dest_id = sld.id
+                LEFT JOIN stock_picking sp ON sm.picking_id = sp.id
+                LEFT JOIN stock_warehouse sw ON sp.picking_type_id = sw.int_type_id
+                WHERE
+                    sml.product_id = 33196 AND
+                    sl.usage = 'internal' AND sld.usage = 'internal'
+            )
+        """)
+
 
     
