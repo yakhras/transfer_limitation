2ad448ba8884cd23b3f570346be4df67c7f35b34
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-09 14:26:55 +0300

    edt

--- stock_flow/models/stock_product_flow_report.py
+++ stock_flow/models/stock_product_flow_report.py
@@ -62,7 +62,7 @@ class StockProductFlowReport(models.Model):
                     LEFT JOIN stock_location sld ON sml.location_dest_id = sld.id
                     LEFT JOIN stock_warehouse sw_out ON sw_out.lot_stock_id = sml.location_id
                     WHERE
-                        sml.company_id = company_id
+                        sml.company_id = %(company_id)s
                         AND sm.state = 'done'
                         AND sl.usage = 'internal' AND sld.usage = 'internal'
 
@@ -95,7 +95,7 @@ class StockProductFlowReport(models.Model):
                     LEFT JOIN stock_location sld ON sml.location_dest_id = sld.id
                     LEFT JOIN stock_warehouse sw_in ON sw_in.lot_stock_id = sml.location_dest_id
                     WHERE
-                        sml.company_id = company_id
+                        sml.company_id = %(company_id)s
                         AND sm.state = 'done'
                         AND sl.usage = 'internal' AND sld.usage = 'internal'
 
@@ -142,7 +142,7 @@ class StockProductFlowReport(models.Model):
                     LEFT JOIN stock_warehouse sw_out ON sw_out.lot_stock_id = sl.id
                     LEFT JOIN stock_warehouse sw_in ON sw_in.lot_stock_id = sld.id
                     WHERE
-                        sml.company_id = company_id
+                        sml.company_id = %(company_id)s
                         AND sm.state = 'done'
                         AND NOT (sl.usage = 'internal' AND sld.usage = 'internal')
                 )
@@ -159,7 +159,7 @@ class StockProductFlowReport(models.Model):
                 FROM move_lines_union
 
             )
-        """)
+        """, {'company_id': company_id})
         self.env.cr.execute("""
             CREATE INDEX IF NOT EXISTS idx_stock_balance_by_product_warehouse
             ON stock_move_line_report (product_id, warehouse_id, date)
