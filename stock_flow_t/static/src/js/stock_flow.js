ee0f9abc1cf6e4eba7897db8c583ff0517352889
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-21 09:54:09 +0300

    edt

--- stock_flow_t/static/src/js/stock_flow.js
+++ stock_flow_t/static/src/js/stock_flow.js
@@ -1,18 +1,14 @@
-odoo.define('stock_flow.stock_flow_refresh', function (require) {
+odoo.define('stock_flow_t.stock_flow_refresh', function (require) {
     "use strict";
     
     var ListController = require('web.ListController');
     
     ListController.include({
-        willStart: function () {
-            var self = this;
+        start: function () {
             if (this.modelName === 'stock.product.flow.report') {
-                // Call refresh method before loading
-                return this._rpc({
+                this._rpc({
                     model: 'stock.product.flow.report',
                     method: 'refresh_materialized_view',
-                }).then(function () {
-                    return self._super.apply(self, arguments);
                 });
             }
             return this._super.apply(this, arguments);
