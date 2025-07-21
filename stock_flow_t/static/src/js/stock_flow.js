ea63d111608cac746fd1620601e9621eab021441
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-21 10:38:51 +0300

    edt

--- stock_flow_t/static/src/js/stock_flow.js
+++ stock_flow_t/static/src/js/stock_flow.js
@@ -1,17 +1,38 @@
+// odoo.define('stock_flow_t.stock_flow_refresh', function (require) {
+//     "use strict";
+    
+//     var ListController = require('web.ListController');
+    
+//     ListController.include({
+//         start: function () {
+//             if (this.modelName === 'stock.product.flow.report') {
+//                 this._rpc({
+//                     model: 'stock.product.flow.report',
+//                     method: 'refresh_materialized_view',
+//                 });
+//             }
+//             return this._super.apply(this, arguments);
+//         }
+//     });
+// });
+
 odoo.define('stock_flow_t.stock_flow_refresh', function (require) {
     "use strict";
     
-    var ListController = require('web.ListController');
+    var AbstractController = require('web.AbstractController');
     
-    ListController.include({
-        start: function () {
+    AbstractController.include({
+        willStart: function () {
+            var self = this;
             if (this.modelName === 'stock.product.flow.report') {
-                this._rpc({
+                return this._rpc({
                     model: 'stock.product.flow.report',
                     method: 'refresh_materialized_view',
+                }).then(function () {
+                    return self._super.apply(self, arguments);
                 });
             }
-            return this._super.apply(this, arguments);
+            return this._super.apply(self, arguments);
         }
     });
 });
