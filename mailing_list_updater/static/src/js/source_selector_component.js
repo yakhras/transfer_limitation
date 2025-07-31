7a71e90477b8ca72465a5670288de4f5e1e6f100
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-31 17:07:16 +0300

    edt

--- mailing_list_updater/static/src/js/source_selector_component.js
+++ mailing_list_updater/static/src/js/source_selector_component.js
@@ -6,7 +6,6 @@ class SourceSelectorComponent extends Component {
 
     setup() {
         this.rpc = this.env.services.rpc;
-        this.company = this.env.services.company;
 
         this.state = useState({
             
@@ -17,7 +16,6 @@ class SourceSelectorComponent extends Component {
             isLoading: false,
             searchTerm: '',
             showSourceDetails: {},
-            companyRecordCounts: {},
         });
 
         this.selectedMailingList = this.props.selectedMailingList;
@@ -200,22 +198,7 @@ class SourceSelectorComponent extends Component {
     toggleSourceDetails(event) {
         const modelName = event.currentTarget.dataset.model;
         this.state.showSourceDetails[modelName] = !this.state.showSourceDetails[modelName];
-        if (this.state.showSourceDetails[modelName] && !this.state.companyRecordCounts[modelName]) {
-            await this.fetchCompanyRecordCount(modelName);
-        }
         event.stopPropagation();
-    }
-
-    async fetchCompanyRecordCount(modelName) {
-        try {
-            const domain = [['company_id', '=', this.company.currentCompany.id]];
-            
-            const count = await this.env.services.orm.searchCount(modelName, domain);
-            this.state.companyRecordCounts[modelName] = count;
-        } catch (error) {
-            console.error(`Failed to get record count for ${modelName}:`, error);
-            this.state.companyRecordCounts[modelName] = 'N/A';
-        }
     }
 
     onSearchInput(event) {
