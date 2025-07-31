df69ac98dd3f4aea46bb49e4d64a3fd563668ee2
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-31 14:36:48 +0300

    edt

--- mailing_list_updater/static/src/js/mailing_list_updater_main.js
+++ mailing_list_updater/static/src/js/mailing_list_updater_main.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import { SourceSelectorComponent } from './source_selector_component.js';
+import { TargetSelectorComponent } from './target_selector_component.js';
 import { FilterBuilderComponent } from './filter_builder_component.js';
 import { PreviewResultsComponent } from './preview_results_component.js';
 import { ProgressTrackerComponent } from './progress_tracker_component.js';
