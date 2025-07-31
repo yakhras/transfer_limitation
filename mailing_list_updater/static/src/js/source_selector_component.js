0f2bee5181ce7ffcaf53cf0456a249bd763e8cbc
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-07-31 18:39:40 +0300

    edt

--- mailing_list_updater/static/src/js/source_selector_component.js
+++ mailing_list_updater/static/src/js/source_selector_component.js
@@ -219,7 +219,7 @@ class SourceSelectorComponent extends Component {
             console.log('Fetching count for model:', modelName);
             console.log('Using domain:', domain);
             
-            const count = await this.rpc({
+            const count = this.rpc({
                 model: modelName,
                 method: 'search_count',
                 args: [domain]
