e237b56e3b922543d5451e2e13f69eb216ee9654
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-08 18:49:11 +0300

    edt

--- mailing_list_updater/models/__init__.py
+++ mailing_list_updater/models/__init__.py
@@ -5,7 +5,7 @@ from . import mailing_list_update_batch
 from . import mailing_contact_tracking
 from . import mailing_filter_template
 from . import mailing_operation_audit
-# from . import mailing_contact
+from . import mailing_contact
 from . import deduplication_engine
 from . import batch_processor
 from . import mailing_list_extension
