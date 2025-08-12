9bce449a344077af3d8d18d97d4db81187fd47b3
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-12 13:12:29 +0300

    edt

--- mailing_list_updater/models/res_partner.py
+++ mailing_list_updater/models/res_partner.py
@@ -5,25 +5,14 @@ from odoo import models, api, _
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    @api.model
+    
     def get_contacts_with_email(self, domain=None):
-        """
-        Search partners with given domain and filter those with email
-        
-        Args:
-            domain: list - Odoo domain filter
-            
-        Returns:
-            dict: Window action to display filtered records
-        """
+        """Update mobile field with count"""
         if domain is None:
             domain = []
         
-        # Add email filter to domain
         email_domain = [('email', '!=', False)] + domain
-        
-        # Search records with email
         partners_with_email = self.search(email_domain)
         
-        # Return tree view action
-        self.mobile = len(partners_with_email)
+        # Write to the record (assuming self is a single record)
+        self.write({'mobile': str(len(partners_with_email))})
