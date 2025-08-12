452df9004360aa66a69509a3056502662a70724e
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-12 15:04:53 +0300

    edt

--- mailing_list_updater/static/src/js/preview_results_component.js
+++ mailing_list_updater/static/src/js/preview_results_component.js
@@ -35,7 +35,7 @@ class PreviewResultsComponent extends Component {
         }
     }
 
-    async onViewClick(modelName) {
+    onViewClick(modelName) {
         console.log('Opening tree view for:', modelName);
 
         const domain = this.getModelDomain(modelName);
@@ -46,7 +46,7 @@ class PreviewResultsComponent extends Component {
             const recordCount = this.env.services.orm.call(modelName, 'search_count', [domain]);
             console.log(`Record count for ${modelName} with domain:`, recordCount);
 
-            const action = await this.env.services.orm.call('res.partner', 'get_contacts_with_email', [domain]);
+            const action = this.env.services.orm.call('res.partner', 'get_contacts_with_email', [domain]);
 
             // Open tree view
             this.env.services.action.doAction({
