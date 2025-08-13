20135cde96ca93531b32947ec0a8a883a82b3735
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-13 11:35:51 +0300

    edt

--- mailing_list_updater/static/src/js/preview_results_component.js
+++ mailing_list_updater/static/src/js/preview_results_component.js
@@ -14,6 +14,7 @@ class PreviewResultsComponent extends Component {
         console.log('Selected mailing list:', this.selectedMailingList);
         this.selectedSources = this.props.selectedSources || [];
         this.filterCriteria = this.props.filterCriteria || {};
+        console.log('Filter criteria:', this.filterCriteria);
     }
 
     onPreviewClick() {
