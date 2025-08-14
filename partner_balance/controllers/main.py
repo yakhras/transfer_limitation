5cea9a587b719d51a2a96b740175dff35f65c996
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-14 14:28:44 +0300

    edt

--- partner_balance/controllers/main.py
+++ partner_balance/controllers/main.py
@@ -80,7 +80,7 @@ class ExcelExport(BaseExcelExport):
             # f"Company: {company_name}" if company_name else "",
             # f"Export Date: {datetime.now().strftime('%Y-%m-%d')}",
             f"Date From: {date_from}" if date_from else "All",
-            f"Date To: {date_to}" if date_to else datetime.now().strftime('%Y-%m-%d'),
+            f"Date To: {date_to}" if date_to else f"Date To: {datetime.now().strftime('%Y-%m-%d')}",
         ]
         return [item for item in header_data if item]
     
