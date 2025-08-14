d74a075d0b97e4f65e3deaf828855b5329826ba5
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-14 12:36:06 +0300

    edt

--- partner_balance/models/account_move_line_excel_report.py
+++ partner_balance/models/account_move_line_excel_report.py
@@ -12,7 +12,7 @@ class AccountMoveLineReport(models.Model):
     _name = 'account.move.line.report'
     _description = 'Account Move Line Report'
     _auto = False
-    _order = 'date DESC'
+    _order = 'date'
 
     date = fields.Date(string='Date', readonly=True)
     move_id = fields.Many2one('account.move', string='Journal Entry', readonly=True)
