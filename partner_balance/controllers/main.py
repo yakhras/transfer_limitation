4c2748f5ed68a8f4b4e1a84f07fb3a2fb552e666
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-14 17:24:35 +0300

    edt

--- partner_balance/controllers/main.py
+++ partner_balance/controllers/main.py
@@ -2,7 +2,7 @@
 
 import json
 import operator
-from datetime import datetime
+from datetime import datetime, timedelta
 import io
 from odoo.tools.misc import xlsxwriter
 from odoo.exceptions import UserError
