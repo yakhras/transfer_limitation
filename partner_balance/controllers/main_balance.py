64c82f42b4e0abf48d75dc1cc3ef42b007046fe7
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-18 12:52:59 +0300

    edt

--- partner_balance/controllers/main_balance.py
+++ partner_balance/controllers/main_balance.py
@@ -120,8 +120,8 @@ class BalanceExcelExport(BaseExportFormat, http.Controller):
             _("Report: %s") % action_name if action_name else "",
             f"Partner: {partner_name}" if partner_name else "",
             f"Company: {company_name}" if company_name else "",
-            f"Export Date: {date.now().strftime('%Y-%m-%d')}",
-            f"Date Range: {date_from or oldest_date} To {date_to or date.now().strftime('%Y-%m-%d')}",
+            f"Export Date: {datetime.datetime.now().strftime('%Y-%m-%d')}",
+            f"Date Range: {date_from or oldest_date} To {date_to or datetime.datetime.now().strftime('%Y-%m-%d')}",
         ]
         return [item for item in header_data if item]
     
