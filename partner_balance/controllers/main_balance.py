a36b75238f3aba66982a70de8cf13594a8a9d332
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-20 10:22:51 +0300

    edt

--- partner_balance/controllers/main_balance.py
+++ partner_balance/controllers/main_balance.py
@@ -175,13 +175,13 @@ class BalanceExcelExport(BaseExportFormat, http.Controller):
 
         # Financial summary row
         xlsx_writer.write(row, 0, "Opening Balance:", xlsx_writer.summary_metric_style)
-        xlsx_writer.worksheet.merge_range(row, 1, row, 2, "2,500,000.00 USD", xlsx_writer.base_style)
+        xlsx_writer.worksheet.merge_range(row, 1, row, 2, "2,500,000.00 USD", xlsx_writer.summary_value_style)
         # xlsx_writer.write(row, 1, "2,500,000.00 USD", xlsx_writer.summary_value_style)
         xlsx_writer.write(row, 3, "Period Movement:", xlsx_writer.summary_metric_style)
-        xlsx_writer.worksheet.merge_range(row, 4, row, 5, "-2,257,012.59 USD", xlsx_writer.base_style)
+        xlsx_writer.worksheet.merge_range(row, 4, row, 5, "-2,257,012.59 USD", xlsx_writer.negative_value_style)
         # xlsx_writer.write(row, 4, "-2,257,012.59 USD", xlsx_writer.negative_value_style)
         xlsx_writer.write(row, 6, "Closing Balance:", xlsx_writer.summary_metric_style)
-        xlsx_writer.worksheet.merge_range(row, 7, row, 8, "242,987.41 USD", xlsx_writer.base_style)
+        xlsx_writer.worksheet.merge_range(row, 7, row, 8, "242,987.41 USD", xlsx_writer.summary_value_style)
         # xlsx_writer.write(row, 7, "242,987.41 USD", xlsx_writer.summary_value_style)
         row += 1
         
