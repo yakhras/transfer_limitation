0c006cb83c5e0ff5b2caa674c759e2d37303501b
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-21 16:03:59 +0300

    edt

--- partner_balance/controllers/main_balance.py
+++ partner_balance/controllers/main_balance.py
@@ -810,6 +810,8 @@ class BalanceGroupExportXlsxWriter(BalanceExportXlsxWriter):
             opening_data.get('credit', 0.0),       # Credit  
             opening_data.get('balance', 0.0),      # Cumulated Balance
             opening_data.get('currency', ''),      # Currency
+            '',
+            '',
         ]
     
 
