5e665be2fbb922fb80bd626e95d3fa5ef51fc7a3
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-22 15:42:14 +0300

    edt

--- partner_balance/controllers/main_balance.py
+++ partner_balance/controllers/main_balance.py
@@ -589,8 +589,8 @@ class BalanceGroupExportXlsxWriter(BalanceExportXlsxWriter):
 
         # Fields that need custom calculation instead of sum
         calculated_fields = {
-            'debit': lambda: aggregates.get('debit', 0) + balance['debit'],
-            'credit': lambda: aggregates.get('credit', 0) + balance['credit'],
+            'debit': lambda: aggregates.get('debit', 0) + balance[3],
+            'credit': lambda: aggregates.get('credit', 0) + balance[4],
             'balance': lambda: aggregates.get('debit', 0) - abs(aggregates.get('credit', 0)),
             'balance_amount': lambda: aggregates.get('debit_amount', 0) - abs(aggregates.get('credit_amount', 0)),
             'cumulated_balance': lambda: aggregates.get('debit', 0) - abs(aggregates.get('credit', 0)),
