495f8884e18d0daca3380ae6674cd0944a5fff4b
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-22 18:08:14 +0300

    edt

--- cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
+++ cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
@@ -853,7 +853,7 @@ export class CashFlowDashboard extends Component {
             name: "Cash Flow Details",
             res_model: "account.move.line",
             domain: [["account_id.code","in",[accountId]]],
-            views: [["list", "form"]],
+            views: [[false, "list"],[false, "form"]],
             target: "current",
             context: {
                 date_from: dateRange.date_from,
