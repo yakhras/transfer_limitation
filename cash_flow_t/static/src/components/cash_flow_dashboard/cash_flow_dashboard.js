530424489147381d0787d2ea38a5eafac062432d
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-24 11:41:53 +0300

    edt

--- cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
+++ cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
@@ -828,34 +828,52 @@ export class CashFlowDashboard extends Component {
 
     viewAccountDetails(accountId) {
         const dateRange = this.getDateRange()
-        const account_codes = accountId.split(', ')
-        const domain = [
-            ["account_id.code","in", account_codes],
-            ["move_id.state", "=", "posted"],
-            ["company_id", "=", this.env.services.company.currentCompany.id],
-        ]
-        if (dateRange.date_from) {
-            domain.push(["date", ">=", dateRange.date_from])
-        }
-        if (dateRange.date_to) {
-            domain.push(["date", "<=", dateRange.date_to])
-        }
+        
         this.actionService.doAction({
             type: "ir.actions.act_window",
             name: "Cash Flow Details",
-            res_model: "account.move.line",
-            domain: domain,
-            views: [[false, "list"],[false, "form"]],
-            view_mode: "list",
+            res_model: "cash.flow.dashboard",
+            res_id: accountId,
+            views: [[false, "form"]],
             target: "current",
             context: {
                 date_from: dateRange.date_from,
                 date_to: dateRange.date_to,
-                period_type: this.state.period,
-                search_default_group_by_partner: 1
+                period_type: this.state.period
             }
         })
     }
+
+    // viewAccountDetails(accountId) {
+    //     const dateRange = this.getDateRange()
+    //     const account_codes = accountId.split(', ')
+    //     const domain = [
+    //         ["account_id.code","in", account_codes],
+    //         ["move_id.state", "=", "posted"],
+    //         ["company_id", "=", this.env.services.company.currentCompany.id],
+    //     ]
+    //     if (dateRange.date_from) {
+    //         domain.push(["date", ">=", dateRange.date_from])
+    //     }
+    //     if (dateRange.date_to) {
+    //         domain.push(["date", "<=", dateRange.date_to])
+    //     }
+    //     this.actionService.doAction({
+    //         type: "ir.actions.act_window",
+    //         name: "Cash Flow Details",
+    //         res_model: "account.move.line",
+    //         domain: domain,
+    //         views: [[false, "list"],[false, "form"]],
+    //         view_mode: "list",
+    //         target: "current",
+    //         context: {
+    //             date_from: dateRange.date_from,
+    //             date_to: dateRange.date_to,
+    //             period_type: this.state.period,
+    //             search_default_group_by_partner: 1
+    //         }
+    //     })
+    // }
 
     getPeriodLabel() {
         const option = this.periodOptions.find(opt => opt.value === this.state.period)
