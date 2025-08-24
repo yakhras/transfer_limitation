18c25ef1887dbb323fa6b88e262db827479ca0c3
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-24 10:55:39 +0300

    edt

--- cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
+++ cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
@@ -846,11 +846,11 @@ export class CashFlowDashboard extends Component {
 // [["account_id.code","in",["120001","120002"]]]
     viewAccountDetails(accountId) {
         const dateRange = this.getDateRange()
-        console.log(this.orm)
         const account_codes = accountId.split(', ')
         const domain = [
             ["account_id.code","in", account_codes],
             ["move_id.state", "=", "posted"],
+            ["company_id", "=", this.env.company.id]
         ]
         console.log(domain)
         
