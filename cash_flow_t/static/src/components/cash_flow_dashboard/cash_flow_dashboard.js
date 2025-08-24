1fbcdf64f705260322977579a2b91fc2378de0a1
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-24 15:57:33 +0300

    edt

--- cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
+++ cash_flow_t/static/src/components/cash_flow_dashboard/cash_flow_dashboard.js
@@ -893,7 +893,7 @@ export class CashFlowDashboard extends Component {
             name: "Cash Flow Details",
             res_model: "account.move.line.report",
             domain: domain,
-            views: [[false, "list"],[false, "form"]],
+            views: [["partner_balance.view_account_move_line_report_tree.id", "list"],[false, "form"]],
             view_mode: "list",
             target: "current",
             context: {
