cb094f37ec963337d75f2cd6fb02142f59381a0f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-27 16:13:10 +0300

    edt

--- partner_balance/models/account_move_line_excel_report.py
+++ partner_balance/models/account_move_line_excel_report.py
@@ -15,7 +15,7 @@ class AccountMoveLineReport(models.Model):
 
     date = fields.Date(string='Date', readonly=True)
     move_id = fields.Many2one('account.move', string='Reference', readonly=True)
-    name = fields.Char(string='Note', readonly=True)
+    # name = fields.Char(string='Note', readonly=True)
     amount_currency = fields.Monetary(string='Amount Currency', readonly=True)
     currency_id = fields.Many2one('res.currency', string='Original Currency', readonly=True)
     debit = fields.Monetary(string='Debit', readonly=True, currency_field='company_currency_id')
