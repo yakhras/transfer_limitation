7e987b1049c7607fa5489f7256294421eb12a0d8
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-28 18:32:16 +0300

    edt

--- partner_balance/models/account_move_line_excel_report.py
+++ partner_balance/models/account_move_line_excel_report.py
@@ -281,7 +281,7 @@ class AccountMoveLineReport(models.Model):
             'debit': 0.00,
             'credit': 0.00,
             'cumulated_balance': 2500000.00,
-            'partner_id': self.env['res.partner'].search([54487], limit=1).id,
+            'partner_id': self.env['res.partner'].search([54487], limit=1),
             'company_id': self.env.company.id,
         })
         
