821da7a4a71ce455ba52c2c307b410e506663f92
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-29 16:02:04 +0300

    edt

--- quanimo_pos_invoice_t/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    property_is_printed_invoice = fields.Boolean('Is Printed Invoice', default=False, company_dependent=True)
