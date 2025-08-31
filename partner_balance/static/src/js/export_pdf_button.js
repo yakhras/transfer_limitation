8f329223560e5ccedbd3fd75e4280cabd723638f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-08-31 17:46:31 +0300

    edt

--- partner_balance/static/src/js/export_pdf_button.js
+++ partner_balance/static/src/js/export_pdf_button.js
@@ -158,7 +158,7 @@ var ExportPdfButtonListController = ListController.extend({
     },
 
     _formatCurrency(value) {
-        return '$' + Number(value || 0).toLocaleString('en-US', {
+        return '₺' + Number(value || 0).toLocaleString('en-US', {
             minimumFractionDigits: 2,
             maximumFractionDigits: 2
         });
