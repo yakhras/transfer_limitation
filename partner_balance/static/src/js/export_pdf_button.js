22be682eefeecb3fefaea3588ad279cd7535cda0
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-01 16:20:06 +0300

    edt

--- partner_balance/static/src/js/export_pdf_button.js
+++ partner_balance/static/src/js/export_pdf_button.js
@@ -74,7 +74,7 @@ var ExportPdfButtonListController = ListController.extend({
         var $clickedTab = $(ev.currentTarget);
         var currencyName = $clickedTab.data('currency');
         
-        console.log('Selected currency:', $clickedTab.data);
+        console.log('Selected currency:', currencyName);
         
         // Update active tab visual state
         this.$('.currency-tab').removeClass('active');
