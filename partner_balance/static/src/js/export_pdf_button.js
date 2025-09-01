c500844b325ebdd6bb912c0bf334dc1103337bf7
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-01 10:45:55 +0300

    edt

--- partner_balance/static/src/js/export_pdf_button.js
+++ partner_balance/static/src/js/export_pdf_button.js
@@ -211,13 +211,13 @@ var ExportPdfButtonListController = ListController.extend({
             model: 'res.partner',
             method: 'action_view_move_line_report_currency',
             // ⬇️ record method => pass a list of ids
-            args: [[partner_id]],
+            args: [[], [partner_id]],
         }).then(action => {
             if (!action) {
                 console.error('Server returned no action');
                 return;
             }
-            this.do_action(action);
+            this.doaAction(action);
         }).catch(err => {
             console.error('RPC error:', err);
         });
