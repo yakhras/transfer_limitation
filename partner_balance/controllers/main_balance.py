c5847ce172a9eca6a5b23751e2dcf8f06b2614b7
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-04 15:53:15 +0300

    edt

--- partner_balance/controllers/main_balance.py
+++ partner_balance/controllers/main_balance.py
@@ -253,8 +253,8 @@ class BalanceExcelExport(BaseExportFormat, http.Controller):
             currency_name = record['currency_id'][1] if record['currency_id'] else 'Unknown'
             balance = record['initial_balance_amount_currency'] or 0
             
-            # if currency_name not in currency_balances:
-            #     currency_balances[currency_name] = 0
+            if currency_name not in currency_balances:
+                currency_balances[currency_name] = 0
             currency_balances[currency_name] = balance
 
         
