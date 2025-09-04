9fb26d9d8a680a08705e62ff7f5ff82239634496
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-04 12:31:15 +0300

    edt

--- partner_balance/static/src/js/export_pdf_button.js
+++ partner_balance/static/src/js/export_pdf_button.js
@@ -256,6 +256,7 @@ var ExportPdfButtonListController = ListController.extend({
         this.update({}, {reload: true});
         var state = this.model.get(this.handle);
         const records = (state && state.data) || [];
+        console.log(records);
         const initialBalance = records.length ? (records[0].data.initial_balance || records[0].data.initial_balance_partner_currency || 0) : 0;
         
         
