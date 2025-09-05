6b119536de0a2fe7558e81239637fdee26a889ad
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-05 18:42:20 +0300

    edt

--- partner_balance/static/src/js/export_pdf_button.js
+++ partner_balance/static/src/js/export_pdf_button.js
@@ -223,7 +223,7 @@ var ExportPdfButtonListController = ListController.extend({
             args: [[], user_id],
         }).then(function(users) {
             console.log(users);
-            this.$('.currency-label').text(users);
+            self.$('.currency-label').text(users);
         });
 
     },
