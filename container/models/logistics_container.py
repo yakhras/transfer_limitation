10494f557ab6775badcafcf2b0a7dbbb5b1adedb
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-14 11:41:01 +0300

    edt

--- container/models/logistics_container.py
+++ container/models/logistics_container.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class LogisticsContainer(models.Model):
