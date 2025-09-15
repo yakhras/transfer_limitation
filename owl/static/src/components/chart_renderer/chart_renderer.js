5fff4e48fdbca57cfa46c007a30a360addb1e45d
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-15 16:15:45 +0300

    edt

--- owl/static/src/components/chart_renderer/chart_renderer.js
+++ owl/static/src/components/chart_renderer/chart_renderer.js
@@ -2,7 +2,8 @@
 
 import { registry } from "@web/core/registry"
 import { loadJS } from "@web/core/assets"
-const { Component, onWillStart, useRef, onMounted } = owl
+const { Component, onWillStart, onMounted } = owl
+const { useRef } = owl.hooks;
 
 export class ChartRenderer extends Component {
     setup(){
