3ecfc2f3ed56c67d93909f85366731c1e54eed83
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-28 11:57:19 +0300

    edt

--- container/models/logistics_bill_lading.py
+++ container/models/logistics_bill_lading.py
@@ -28,7 +28,7 @@ class LogisticsBillLading(models.Model):
     purchase_order_ids = fields.One2many('purchase.order', 'bill_lading_id', string='Purchase Orders')
     
     # Direct container relation
-    container_ids = fields.Many2many('logistics.container', 'bill_lading_container_rel', 
+    container_ids = fields.One2many('logistics.container', 'bill_lading_container_rel', 
                                     'bill_lading_id', 'container_id', string='Containers')
     
     # Parties Information
