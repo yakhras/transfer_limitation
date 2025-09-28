f4d55fdece1c9c943f6e74df5c461bde9bdc4647
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-28 15:41:08 +0300

    edt

--- container/models/logistics_container.py
+++ container/models/logistics_container.py
@@ -146,22 +146,22 @@ class LogisticsContainer(models.Model):
                                   container.other_charges)
     
 
-    @api.constrains('requisition_id', 'purchase_order_ids')
-    def _check_purchase_order_requisition_consistency(self):
-        for container in self:
-            for purchase_order in container.purchase_order_ids:
-                if purchase_order.requisition_id not in container.requisition_id:
-                    raise ValidationError(_(
-                        'Container %s: Purchase Order %s must belong to the same requisition (%s).'
-                    ) % (container.name, purchase_order.name, container.requisition_id.name))
+    # @api.constrains('requisition_id', 'purchase_order_ids')
+    # def _check_purchase_order_requisition_consistency(self):
+    #     for container in self:
+    #         for purchase_order in container.purchase_order_ids:
+    #             if purchase_order.requisition_id not in container.requisition_id:
+    #                 raise ValidationError(_(
+    #                     'Container %s: Purchase Order %s must belong to the same requisition (%s).'
+    #                 ) % (container.name, purchase_order.name, container.requisition_id.name))
     
-    @api.constrains('bill_lading_id', 'requisition_id')
-    def _check_bill_lading_requisition_consistency(self):
-        for container in self:
-            if container.bill_lading_id and container.bill_lading_id.requisition_id not in container.requisition_id:
-                raise ValidationError(_(
-                    'Container %s: Bill of Lading must belong to the same requisition (%s).'
-                ) % (container.name, container.requisition_id.name))
+    # @api.constrains('bill_lading_id', 'requisition_id')
+    # def _check_bill_lading_requisition_consistency(self):
+    #     for container in self:
+    #         if container.bill_lading_id and container.bill_lading_id.requisition_id not in container.requisition_id:
+    #             raise ValidationError(_(
+    #                 'Container %s: Bill of Lading must belong to the same requisition (%s).'
+    #             ) % (container.name, container.requisition_id.name))
     
 
     @api.onchange('requisition_id')
