44ee75e8120c209398672ed49cc76d1fe6c327bd
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-29 10:05:59 +0300

    edt

--- container/models/logistics_bill_lading.py
+++ container/models/logistics_bill_lading.py
@@ -44,6 +44,7 @@ class LogisticsBillLading(models.Model):
     
     # Shipping Information
     forwarder_id = fields.Many2one('res.partner', string='Forwarding Agent', tracking=True)
+    shipping_line_id = fields.Many2one('res.partner', string='Shipping Line', tracking=True)
     vessel_name = fields.Char('Vessel Name', tracking=True)
     voyage_number = fields.Char('Voyage Number', tracking=True)
     port_of_loading_id = fields.Many2one('res.country.state', string='Port of Loading', tracking=True)
