744cb1f21be3a09ed8d4ee4c9a68ace0a2cfde58
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-09-30 12:03:52 +0300

    edt

--- container/models/logistics_bill_lading.py
+++ container/models/logistics_bill_lading.py
@@ -37,7 +37,7 @@ class LogisticsBillLading(models.Model):
     purchase_order_ids = fields.Many2many('purchase.order', string='Purchase Orders')
     
     # Direct container relation
-    container_ids = fields.Many2many(
+    container_ids = fields.Many2one(
         'logistics.container', 
         string='Containers',
         domain="[('requisition_id', 'in', requisition_id)]"  # Only containers from selected requisitions
