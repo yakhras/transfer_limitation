660626f3971eb424b1f94e9c7ad4508c46c8a320
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-10-15 12:06:44 +0300

    edt

--- partner_balance_t/models/partner_statement.py
+++ partner_balance_t/models/partner_statement.py
@@ -302,7 +302,7 @@ class PartnerStatement(models.Model):
                 section['products'] = invoiced_data.get('sale_orders', [])
 
                 invoices = self.get_invoices_from_orders(order)
-                section['invoices'] = invoices.sorted(key=lambda x: x['invoice_date'])
+                section['invoices'] = invoices.sort(key=lambda x: x['invoice_date'])
                 
                 payment_data = self._get_order_payments(order)
                 section['payments'] = payment_data
