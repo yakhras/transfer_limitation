e2cca955673c2a7198ad35813de749e1c888ae1f
yaserakhras <58257996+yakhras@users.noreply.github.com>
2025-10-15 18:00:26 +0300

    edt

--- partner_balance_t/__manifest__.py
+++ partner_balance_t/__manifest__.py
@@ -27,7 +27,6 @@
     'data': [
         'security/ir.model.access.csv',
         "views/partner_balance_view.xml",
-        "views/template_style.xml",
         "views/template_new.xml",
         
 
